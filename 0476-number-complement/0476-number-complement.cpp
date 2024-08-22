02df958eb8cea3222f1f8584fa964b24aea61512
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-08-22 12:07:11 +0530

    Time: 0 ms (100%), Space: 7.5 MB (57.13%) - LeetHub

--- 0476-number-complement/0476-number-complement.cpp
+++ 0476-number-complement/0476-number-complement.cpp
@@ -2,9 +2,13 @@ class Solution {
 public:
     int findComplement(int num) {
         //using bitmasking
-        //O(logn) & O(1)
-        unsigned mask = ~0;
-        while(mask & num) mask = mask << 1;
-        return ~num ^ mask;
+        //O(1) & O(1)
+        if(num==0) return 1;
+        unsigned int mask = ~0; //~bitwise not operator
+        while(num & mask){ //bitwise and operator is both bits are 1 then result is 1
+            mask<<=1;
+        }
+        return ~mask ^ num;
+        
     }
 };
