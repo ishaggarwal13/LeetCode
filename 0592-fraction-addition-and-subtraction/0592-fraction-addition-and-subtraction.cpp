00afaa277bc9908647ba640bb8547e766e95cf2c
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-08-23 08:56:19 +0530

    Time: 3 ms (50.39%), Space: 7.8 MB (58.91%) - LeetHub

--- 0592-fraction-addition-and-subtraction/0592-fraction-addition-and-subtraction.cpp
+++ 0592-fraction-addition-and-subtraction/0592-fraction-addition-and-subtraction.cpp
@@ -2,38 +2,17 @@ class Solution {
 public:
     //O(n) and O(1)
     string fractionAddition(string expression) {
-        int numerator=0, denominator=1; //intially 0/1 
-        int i=0, n=expression.size();
+        istringstream in(expression); //Create an input string stream
+        int A=0, B=1, a, b;
+        char _; //Temporary variable to store the '/' character in the string
+        while(in >> a >> _ >> b){ //extract numerator, / & denominator from the stream
+            A = A * b + B * a;
+            B = B * b;
+            int g = abs(__gcd(A, B));
+            A /= g;
+            B /= g;
+        } 
 
-        while(i<n){
-            int sign = 1;
-            if(expression[i] == '-' || expression[i] == '+' ){
-                if(expression[i] == '-') sign = -1;
-                i++;
-            }
-
-            int num=0;
-            while(i<n && isdigit(expression[i])){
-                num = num * 10 + (expression[i] - '0');
-                i++;
-            }
-            num *= sign;
-
-            i++; //skip / chracter
-
-            int den=0;
-            while(i<n && isdigit(expression[i])){
-                den = den * 10 + (expression[i] - '0');
-                i++;
-            }
-
-            numerator = den*numerator + num*denominator;
-            denominator = den * denominator;
-
-            int gcdval = gcd(abs(numerator), denominator);
-            numerator /= gcdval;
-            denominator /= gcdval;
-        }
-        return to_string(numerator) + "/" + to_string(denominator);
+        return to_string(A) + "/" + to_string(B);
     }
 };
