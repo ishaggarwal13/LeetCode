7ef1213d0164608c06be441902f5c823ff3cf459
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-08-25 01:52:40 +0530

    Time: 19 ms (43.76%), Space: 18.5 MB (31.36%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -12,39 +12,28 @@ class Solution {
 public:
     ListNode* mergeTwoSorted(ListNode *l1, ListNode* l2)
     {
-        ListNode* dummy = new ListNode(-1);
-        ListNode* last = dummy;
-        
-        while(l1 and l2)
-        {
-            if(l1->val <= l2->val){
-                last->next = l1;
-                last = l1;
-                l1=l1->next;
-            }
-            else{
-                last->next = l2;
-                last = l2;
-                l2=l2->next;
-            }
+        if(l1 == nullptr) return l2;
+        if(l2 == nullptr) return l1;
+       
+        if(l1->val <= l2->val) {
+            l1->next = mergeTwoSorted(l1->next, l2);
+            return l1;
+        } else {
+            l2->next = mergeTwoSorted(l1, l2->next);
+            return l2;
         }
-        
-        if(!l1)
-            last->next = l2;
-        else
-            last->next = l1;
-        return dummy->next;
     }
     
     ListNode* mergeKLists(vector<ListNode*>& lists)
     {
-        int n=lists.size();
-        if(lists.size()==0) return NULL;
-        while(n>1){
-            
-            for(int i=0;i<n/2;i++)
-                lists[i] = mergeTwoSorted(lists[i], lists[n-i-1]);
-            n = (n+1)/2;
+        int len = lists.size();
+        if(len == 0) return NULL;
+
+        while(len>1){
+            for(int i=0; i<len/2; i++){
+                lists[i] = mergeTwoSorted(lists[i], lists[len-i-1]);
+            }
+            len = (len+1)/2;
         }
         return lists.front();
     }
