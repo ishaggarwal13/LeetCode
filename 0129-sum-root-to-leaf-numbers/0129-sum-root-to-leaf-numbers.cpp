178ccc413058b27814c1d7a8abf929a55565ff14
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-08-31 00:26:45 +0530

    Time: 0 ms (100%), Space: 11.3 MB (28.35%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.cpp
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.cpp
@@ -11,23 +11,23 @@
  */
 class Solution {
 public:
-    //iterative using bfs using queue O(N) & O(H)~O(N)
-    int sumNumbers(TreeNode* root) {
-        int totalsum = 0;
-        queue<pair<TreeNode*, int>> q;
-        q.push({root, 0});
-
-        while(!q.empty()){
-            auto [root, currsum] = q.front();
-            q.pop();
-
-            currsum = currsum*10 + root->val;
-            if(!root->left && !root->right) totalsum += currsum;
-
-            if(root->left) q.push({root->left, currsum});
-            if(root->right) q.push({root->right, currsum});
+    //inorder
+    void inorder(TreeNode* root, int& ans, int sum){
+        if(!root) return;
+        //check for leaf node
+        if(root->left == NULL && root->right == NULL){
+            sum = sum*10 + root->val;
+            ans += sum;
+            sum /= 10;
         }
-
-        return totalsum;
+        sum = sum*10 + root->val;
+        inorder(root->left, ans, sum);
+        inorder(root->right, ans, sum);
+        sum = sum/10; //for backtrack reduce the last element
+    }
+    int sumNumbers(TreeNode* root) {
+        int ans=0;
+        inorder(root, ans, 0);
+        return ans;
     }
 };
