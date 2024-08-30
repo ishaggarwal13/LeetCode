f2597f2bb456de23e5f9db366bb477355aecf675
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-08-30 23:49:55 +0530

    Time: 594 ms (61.37%), Space: 245.8 MB (55.79%) - LeetHub

--- 2803-modify-graph-edge-weights/2803-modify-graph-edge-weights.cpp
+++ 2803-modify-graph-edge-weights/2803-modify-graph-edge-weights.cpp
@@ -1,59 +1,82 @@
 class Solution {
 public:
-    vector<vector<int>> modifiedGraphEdges(int n, vector<vector<int>>& edges, int source, int destination, int target) {
-        vector<vector<pair<int, int>>> adjacencyList(n);
-        for (int i = 0; i < edges.size(); i++) {
-            int nodeA = edges[i][0], nodeB = edges[i][1];
-            adjacencyList[nodeA].emplace_back(nodeB, i);
-            adjacencyList[nodeB].emplace_back(nodeA, i);
-        }
-
-        vector<vector<int>> distances(n, vector<int>(2, INT_MAX));
-        distances[source][0] = distances[source][1] = 0;
-
-        runDijkstra(adjacencyList, edges, distances, source, 0, 0);
-        int difference = target - distances[destination][0];
-        if (difference < 0) return {}; 
-        runDijkstra(adjacencyList, edges, distances, source, difference, 1);
-        if (distances[destination][1] < target) return {}; 
-
-        for (auto& edge : edges) {
-            if (edge[2] == -1) edge[2] = 1;
-        }
-        return edges;
-    }
-
-private:
-    void runDijkstra(const vector<vector<pair<int, int>>>& adjacencyList, vector<vector<int>>& edges, vector<vector<int>>& distances, int source, int difference, int run) {
-        int n = adjacencyList.size();
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> priorityQueue;
-        priorityQueue.push({0, source});
-        distances[source][run] = 0;
-
-        while (!priorityQueue.empty()) {
-            auto [currentDistance, currentNode] = priorityQueue.top();
-            priorityQueue.pop();
-
-            if (currentDistance > distances[currentNode][run]) continue;
-
-            for (auto& neighbor : adjacencyList[currentNode]) {
-                int nextNode = neighbor.first, edgeIndex = neighbor.second;
-                int weight = edges[edgeIndex][2];
-
-                if (weight == -1) weight = 1; 
-
-                if (run == 1 && edges[edgeIndex][2] == -1) {
-                    int newWeight = difference + distances[nextNode][0] - distances[currentNode][1];
-                    if (newWeight > weight) {
-                        edges[edgeIndex][2] = weight = newWeight;
-                    }
-                }
-
-                if (distances[nextNode][run] > distances[currentNode][run] + weight) {
-                    distances[nextNode][run] = distances[currentNode][run] + weight;
-                    priorityQueue.push({distances[nextNode][run], nextNode});
+    vector<pair<int,int>>dp[200];
+    vector<int>find(int n,int src,int dist)
+    {
+        vector<int>dis(n+1,1e9+10);
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+        pq.push({0,src});
+        dis[src]=0;
+        vector<int>vis(n+10,0);
+        while(pq.size())
+        {
+            auto [wt,node]=pq.top();
+            pq.pop();
+            if(vis[node])
+            {
+                continue;
+            }
+            vis[node]=1;
+            for(auto &[current_node,current_wt]:dp[node])
+            {
+                if(dis[current_node]>current_wt+wt)
+                {
+                    dis[current_node]=current_wt+wt;
+                    pq.push({dis[current_node],current_node});
                 }
             }
         }
+        return dis;
+    }
+    vector<vector<int>> modifiedGraphEdges(int n, vector<vector<int>>& edges, int src, int dest, int target) 
+    {
+        for(auto &it:edges)
+        {
+            if(it[2]==-1)
+            {
+                continue;
+            }
+            dp[it[0]].push_back({it[1],it[2]});
+            dp[it[1]].push_back({it[0],it[2]});
+        }
+        vector<int>dist=find(n,src,dest);
+        if(dist[dest]<target)
+        {
+            return {};
+        }
+       if(dist[dest]==target)
+       {
+           for(auto &it:edges)
+           {
+               if(it[2]==-1){
+                   it[2]=2e9;
+               }
+           }
+           return edges;
+       }
+        for(int i=0;i<edges.size();i++)
+        {
+            int u=edges[i][0],v=edges[i][1],w=edges[i][2];
+            if(w==-1)
+            {
+                edges[i][2]=1;
+                dp[u].push_back({v,1});
+                dp[v].push_back({u,1});
+                vector<int>dis=find(n,src,dest);
+                if(dis[dest]<=target)
+                {
+                    edges[i][2]+=(target-dis[dest]);
+                    for(int j=i+1;j<edges.size();j++)
+                    {
+                        if(edges[j][2]==-1)
+                        {
+                            edges[j][2]=2e9;
+                        }
+                    }
+                    return edges;
+                }
+            }
+        }
+        return {};
     }
 };
