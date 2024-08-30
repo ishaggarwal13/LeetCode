a7300568e15d0cb27cfcad736b2b769ab44f50db
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-08-30 23:25:35 +0530

    Time: 7 ms (71.22%), Space: 19.9 MB (78.23%) - LeetHub

--- 0112-path-sum/0112-path-sum.cpp
+++ 0112-path-sum/0112-path-sum.cpp
@@ -13,10 +13,14 @@ class Solution {
 public:
     bool hasPathSum(TreeNode* root, int targetSum) {
         if(!root) return false;
-        targetSum -= root->val; //sub the node value from target
-        //check for leaft nodes
-        if(!root->left && !root->right) return targetSum == 0;
-
-        return hasPathSum(root->left, targetSum) || hasPathSum(root->right, targetSum);
+        
+        if (!root->left && !root->right) {
+            return targetSum == root->val;
+        }
+        
+        bool left_sum = hasPathSum(root->left, targetSum - root->val);
+        bool right_sum = hasPathSum(root->right, targetSum - root->val);
+        
+        return left_sum || right_sum;
     }
 };
