ee2faa247f5723e9595b5265c6050542d8dc846b
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-09-01 10:56:57 +0530

    Time: 89 ms (30.54%), Space: 92.6 MB (41.62%) - LeetHub

--- 2132-convert-1d-array-into-2d-array/2132-convert-1d-array-into-2d-array.cpp
+++ 2132-convert-1d-array-into-2d-array/2132-convert-1d-array-into-2d-array.cpp
@@ -8,8 +8,7 @@ public:
         int k=0;
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
-                ans[i][j] = original[k];
-                k++;
+                ans[i][j] = original[k++];
             }
         }
         return ans;
