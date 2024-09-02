acd2c5e4090d891c601b0f40f56d7fb7fab7d849
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-09-02 17:32:16 +0530

    Time: 104 ms (22.81%), Space: 77 MB (18.08%) - LeetHub

--- 2006-find-the-student-that-will-replace-the-chalk/2006-find-the-student-that-will-replace-the-chalk.cpp
+++ 2006-find-the-student-that-will-replace-the-chalk/2006-find-the-student-that-will-replace-the-chalk.cpp
@@ -1,10 +1,7 @@
 class Solution {
 public:
     int chalkReplacer(vector<int>& chalk, int k) {
-        long long sum = 0;
-        for(int i=0; i<chalk.size(); i++){
-            sum += chalk[i];
-        }
+        long long sum = accumulate(chalk.begin(), chalk.end(), 0ll);
 
         k %= sum;
 
