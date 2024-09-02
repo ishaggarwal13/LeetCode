d4d68ec11eaf440474b34e2d465ef837f907cc51
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-09-02 23:45:57 +0530

    Time: 811 ms (6.53%), Space: 0B (100%) - LeetHub

--- 1948-rearrange-products-table/1948-rearrange-products-table.sql
+++ 1948-rearrange-products-table/1948-rearrange-products-table.sql
@@ -1,13 +1,7 @@
-# Write your MySQL query statement below
-# UNION problem
-select product_id, 'store1' as store, store1 as price
+/* Write your T-SQL query statement below */
+select product_id, store, price
 from products
-where store1 is not null
-UNION
-select product_id, 'store2' as store, store2 as price
-from products
-where store2 is not null
-UNION
-select product_id, 'store3' as store, store3 as price
-from products
-where store3 is not null
+UNPIVOT(
+    price
+    for store in (store1, store2, store3)
+) as tmp
