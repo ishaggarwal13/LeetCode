7cd7b02099ee5005581ce58cbb5e988faf1ab00a
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-09-04 00:45:22 +0530

    Time: 5 ms (31.12%), Space: 10.3 MB (57.51%) - LeetHub

--- 2436-make-array-zero-by-subtracting-equal-amounts/2436-make-array-zero-by-subtracting-equal-amounts.cpp
+++ 2436-make-array-zero-by-subtracting-equal-amounts/2436-make-array-zero-by-subtracting-equal-amounts.cpp
@@ -1,13 +1,19 @@
 class Solution {
 public:
     int minimumOperations(vector<int>& nums) {
-        //return the total of unique number exsist in array rather than 0
-        unordered_set<int> mp;
-        for(auto i: nums){
-            if(i != 0){
-                mp.insert(i);
+        //brute force - O(n^2)
+        sort(nums.begin(), nums.end());
+        int count=0;
+
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i]>0){
+                int x = nums[i];
+                count++;
+                for(int j=i; j<nums.size(); j++){
+                    nums[j] = nums[j] - x;
+                }
             }
         }
-        return mp.size();
+        return count;
     }
 };
