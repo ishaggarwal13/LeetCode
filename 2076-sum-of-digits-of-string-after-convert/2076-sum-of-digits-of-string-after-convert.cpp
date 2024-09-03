c28b239ed345e56faf756507df598068efc1fe4e
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-09-03 22:56:01 +0530

    Time: 0 ms (100%), Space: 8 MB (77.66%) - LeetHub

--- 2076-sum-of-digits-of-string-after-convert/2076-sum-of-digits-of-string-after-convert.cpp
+++ 2076-sum-of-digits-of-string-after-convert/2076-sum-of-digits-of-string-after-convert.cpp
@@ -1,20 +1,25 @@
 class Solution {
 public:
     int getLucky(string s, int k) {
-        string number = "";
-        for (char x : s) {
-            number += to_string(x - 'a' + 1);
+        int num=0;
+        // convert letters to digits
+        for (char c: s){
+            int x=c-'a'+1;
+            auto [q, r]=div(x, 10);// x may has 1~2 digits
+            num+=q+r;
         }
-        
-        // Perform the transformation `k` times
-        while (k > 0) {
-            int temp = 0;
-            for (char x : number) {
-                temp += x - '0';  // Sum the digits of the current number
+    //    cout<<num<<endl;
+        if (k==1) return num;
+        k--;
+        for(int x=num; x>=10 && k>0; k--){
+            for(num=0; x>0; ){
+                auto [q, r]=div(x, 10);
+                num+=r;
+                x=q;
             }
-            number = to_string(temp);  // Convert the sum back to a string
-            k--;
+    //        cout<<"k="<<k<<", num="<<num<<endl;
+            x=num;
         }
-        return stoi(number);
+        return num;
     }
 };
