281b78286ed1078529d01548a49df11cb14fe895
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-09-03 23:52:20 +0530

    Time: 3 ms (36.81%), Space: 8.6 MB (44.58%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.cpp
+++ 0228-summary-ranges/0228-summary-ranges.cpp
@@ -1,27 +1,16 @@
 class Solution {
 public:
-    void pushRes(vector<string>& res, int start, int end){
-        if(start != end){
-            res.push_back(to_string(start) + "->" + to_string(end));
-        } else {
-            res.push_back(to_string(start));
-        }
-    }
     vector<string> summaryRanges(vector<int>& nums) {
         vector<string> res;
-        if(nums.size() == 0) return res;
-        int start = nums[0], end = nums[0];
-
-        for(int i=1; i<nums.size(); i++){
-            if(nums[i] == 1+nums[i-1]){
-                end = nums[i]; // inc end till the end of continous range
-            } else{
-                pushRes(res, start, end); //otherwise add the start and end of range 1 into array
-                start = nums[i]; //assign both start and end to curr element
-                end = nums[i];
-            }
+        int n = nums.size(), i=0, j;
+        while(i<n){
+            j = i+1; //moving j right
+            while(j<n && nums[j-1] + 1 == nums[j]) j++; //increasing j
+            //composing and appending the next element to res 
+            res.push_back( to_string(nums[i]) + (j-i > 1 ? "->" + to_string(nums[j-1]) : "") );
+            //settle i for the next element for new range to j
+            i = j;
         }
-        pushRes(res, start, end);
         return res;
     }
 };
