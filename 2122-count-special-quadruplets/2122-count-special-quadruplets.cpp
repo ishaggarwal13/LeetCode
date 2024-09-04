857666d238e92d78936f519560e689100278c864
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-09-04 12:43:37 +0530

    Time: 79 ms (5.1%), Space: 18 MB (5.63%) - LeetHub

--- 2122-count-special-quadruplets/2122-count-special-quadruplets.cpp
+++ 2122-count-special-quadruplets/2122-count-special-quadruplets.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
     int countQuadruplets(vector<int>& arr) {
-        //O(n^3) and O(n)
+        //O(n^2) and O(n)
         int c=0,n=arr.size();
-         for(int i=0;i<n-3;i++){
-             for(int j=i+1;j<n-2;j++){
-                 for(int k=j+1;k<n-1;k++){
-                     for(int l=k+1;l<n;l++){
-                         if(arr[i]+arr[j]+arr[k]==arr[l])c++;
-                     }
+        map<int,int>mp;
+       
+         for(int i=n-1;i>=2;i--){
+             for(int j=i-1;j>=1;j--){
+                 for(int k=j-1;k>=0;k--){  
+                     if(mp[arr[i]+arr[j]+arr[k]])
+                         c+=mp[arr[i]+arr[j]+arr[k]];
+                    
                  }
              }
+             mp[arr[i]]++;
+             
          }
          return c;
     }
