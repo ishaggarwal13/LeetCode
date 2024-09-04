ba2896565fd1f4a91a795591d0e7f6fc3df45872
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-09-04 22:35:31 +0530

    Time: 125 ms (22.1%), Space: 44 MB (35.91%) - LeetHub

--- 0906-walking-robot-simulation/0906-walking-robot-simulation.cpp
+++ 0906-walking-robot-simulation/0906-walking-robot-simulation.cpp
@@ -2,36 +2,45 @@ class Solution {
 public:
     int robotSim(vector<int>& commands, vector<vector<int>>& obstacles) {
         //time complexity is O(N + K) and space complexity is O(K), 
-        unordered_set<string> s;
-        for(auto& obstacle : obstacles){
-            s.insert( to_string(obstacle[0]) + "," + to_string(obstacle[1]) );
+        unordered_map<string,int>mp;
+        for(auto it:obstacles){
+            string key = to_string(it[0])+"+"+to_string(it[1]);
+            mp[key]++;
         }
+        vector<vector<int>>direc = {{-1,0},{0,1},{1,0},{0,-1}}; 
+        // according to order W N E S
 
-        int x=0, y=0, dx=0, dy=1; //(dx,dy) (0,1) for north position
-        int ans=0;
-
-        for(auto i : commands){
-            if(i == -1){
-                int temp = dx;
-                dx = dy;
-                dy = -temp;
-            } else if(i == -2){
-                int temp = dx;
-                dx = -dy;
-                dy = temp;
-            } else {
-                for(int j = 0; j<i; j++){
-                    int xx = x + dx;
-                    int yy = y + dy;
-
-                    if(s.find( to_string(xx) + "," + to_string(yy) ) != s.end())
+        int dir = 1; // i.e. north 
+        // 0: west, 1: north, 2: east, 3: south
+        int x=0,y=0;
+        int mxDist=0;
+        for(int it:commands){
+            if(it==-2){
+                // move left
+                if(dir==0) dir = 3;
+                else dir--;
+            }
+            else if(it==-1){
+                // move right
+                dir = (dir+1)%4;
+            }
+            else{
+                int k = it;
+                for(int i=1; i<=k; i++){
+                    int newX = x+direc[dir][0];
+                    int newY = y+direc[dir][1];
+                    
+                    string key = to_string(newX)+"+"+to_string(newY);
+                    if(mp.find(key)!=mp.end()){
+                        // obstacle has been found so don't go there
                         break;
-                    x = xx;
-                    y = yy;
-                    ans = max(ans, x*x + y*y);
+                    }
+                    x = newX, y=newY;
+                    mxDist = max(mxDist, x*x+y*y);
                 }
             }
-        }
-        return ans;
+        } 
+
+        return mxDist;
     }
 };
