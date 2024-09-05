49a1d749be813582a481a5190c8f5b68ee499160
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-09-06 01:41:35 +0530

    Time: 4 ms (79.18%), Space: 12.3 MB (38.77%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -1,16 +1,13 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        //O(n) & O(n) map
-        unordered_map<int, int> mp;
-        for(auto i : nums){
-            mp[i]++;
-        }
-        for(auto i : mp){
-            if(i.second == 1){
-                return i.first;
+        //O(n) & O(n) using map
+        sort(nums.begin(), nums.end());
+        for(int i=1; i<nums.size(); i+=3){
+            if(nums[i] != nums[i-1]){
+                return nums[i-1];
             }
         }
-        return -1;
+        return nums[nums.size()-1];
     }
 };
