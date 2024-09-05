ce512bda682baaf5a44f5c01508ca71ed8f79293
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-09-06 00:59:56 +0530

    Time: 24 ms (18.04%), Space: 23.9 MB (14.39%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,15 +1,19 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        unordered_map<int, int> mp;
-        for(auto num : nums){
-            mp[num]++;
+        //preffix sum by using set
+        unordered_set<int> s;
+        for(auto i : nums){
+            s.insert(i);
         }
-        for(auto i : mp){
-            if(i.second == 1){
-                return i.first;
-            }
+        //sum of unique elemets and double it 
+        int sum=0;
+        for(auto i : s){
+            sum += i;
         }
-        return -1;
+        sum = sum*2;
+        //now sub the nums array sum from double unique elemets sum as other elemet appear two times
+        int sumOriginal = accumulate(nums.begin(), nums.end(), 0);
+        return sum - sumOriginal;
     }
 };
