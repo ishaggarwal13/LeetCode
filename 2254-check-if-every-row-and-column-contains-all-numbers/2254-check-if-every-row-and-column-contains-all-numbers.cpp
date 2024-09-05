526808c094eafc0999ad9b93d8916dad4a35a395
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-09-06 01:53:52 +0530

    Time: 195 ms (58.6%), Space: 95.1 MB (47.02%) - LeetHub

--- 2254-check-if-every-row-and-column-contains-all-numbers/2254-check-if-every-row-and-column-contains-all-numbers.cpp
+++ 2254-check-if-every-row-and-column-contains-all-numbers/2254-check-if-every-row-and-column-contains-all-numbers.cpp
@@ -2,23 +2,13 @@ class Solution {
 public:
     bool checkValid(vector<vector<int>>& matrix) {
         int n = matrix.size();
-        unordered_set<int> s1, s2;
         for(int i=0; i<n; i++){
+            unordered_set<int> s1, s2;
             for(int j=0; j<n; j++){
-                if(s1.find(matrix[i][j]) != s1.end()){ //column (0,0), (0,1), ...
-                   return false; //same element exsit already
-                } else {
-                     s1.insert(matrix[i][j]);
-                }
-
-                if(s2.find(matrix[j][i]) != s2.end()){ //row (0,0), (1,0), ...
-                   return false; //same element exsit already
-                } else {
-                     s2.insert(matrix[j][i]);
-                }
+                s1.insert(matrix[i][j]); //row
+                s2.insert(matrix[j][i]); //column
             }
-            s1.clear();
-            s2.clear();    
+            if(s1.size() != n || s2.size() != n) return false;   
         }
         return true;
     }
