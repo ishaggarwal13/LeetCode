e5c109b196f0ef430247330c8cdecab483549ab5
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-09-05 16:07:10 +0530

    Time: 100 ms (50%), Space: 116.3 MB (69.12%) - LeetHub

--- 2155-find-missing-observations/2155-find-missing-observations.cpp
+++ 2155-find-missing-observations/2155-find-missing-observations.cpp
@@ -1,17 +1,24 @@
 class Solution {
 public:
     vector<int> missingRolls(vector<int>& rolls, int mean, int n) {
+        //O(n) & O(1) greedy
         int sum1 = accumulate(rolls.begin(), rolls.end(), 0);
         int m = rolls.size();
         int sum2 = mean*(n+m) - sum1;
 
         if(sum2>6*n || sum2<n) return {};
 
-        int q = sum2/n; //distribution evenly
-        int r = sum2%n; //how many 1 we need to add to number to makerolls up sum to sum2
-        vector<int> ans(n, q); //declaring vector of size n and value to it as q
-        for(int i=1; i<=r; i++){
-            ans[i]++;
+        vector<int> ans(n, 1); //asigning every value in n size vector to 1
+        sum2 -= n; //removing 1's n from the sum
+        int i=0;
+        while(sum2>0){
+            while(sum2>0 && ans[i]<6){
+                ans[i]++;
+                sum2--;
+            }
+            if(sum2>0){
+                i++;
+            }
         }
         return ans;
     }
