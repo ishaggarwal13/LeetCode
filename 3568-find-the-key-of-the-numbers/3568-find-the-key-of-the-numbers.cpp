7258fdba0df03557584a2a4c4bc67ab96c415afb
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-09-06 00:46:35 +0530

    Time: 0 ms (100%), Space: 10.4 MB (13.65%) - LeetHub

--- 3568-find-the-key-of-the-numbers/3568-find-the-key-of-the-numbers.cpp
+++ 3568-find-the-key-of-the-numbers/3568-find-the-key-of-the-numbers.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     int generateKey(int num1, int num2, int num3) {
-        int s1 = num1%10, s2 = num2%10, s3 = num3%10;
-        num1 = num1/10;
-        num2 = num2/10;
-        num3 = num3/10;
-        int f1 = num1%10, f2 = num2%10, f3 = num3%10;
-        num1 = num1/10;
-        num2 = num2/10;
-        num3 = num3/10;
-        int e1 = num1%10, e2 = num2%10, e3 = num3%10;
-        num1 = num1/10;
-        num2 = num2/10;
-        num3 = num3/10;
-        int res = min(s1, min(s2,s3))*1 + min(f1, min(f2,f3))*10 + min(e1, min(e2,e3))*100
-                    + min(num1, min(num2,num3))*1000;
-        return res;
+        //string conversion
+        string s = "0000", res = "";
+        string s1 = to_string(num1);
+        string s2 = to_string(num2); 
+        string s3 = to_string(num3);
+
+        s1 = s.substr(0, s.size()-s1.size()) + s1;
+        s2 = s.substr(0, s.size()-s2.size()) + s2;
+        s3 = s.substr(0, s.size()-s3.size()) + s3;
+
+        for(int i=0; i<s.size(); i++){
+            res += min(s1[i], min(s2[i], s3[i]));
+        }
+        
+        return stoi(res); //string to int conversion
     }
 };
