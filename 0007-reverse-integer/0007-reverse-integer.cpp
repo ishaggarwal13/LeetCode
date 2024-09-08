f53962c34b7a10ef62da132c5f12f7f32b7bd2be
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-09-08 23:31:00 +0530

    Time: 0 ms (100%), Space: 8.3 MB (45.11%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int reverse(int x) {
+        long r = 0;
+        while(x){
+            r = r*10 + x%10; //extracting last digit by % and multiply by 10 
+            x = x/10;
+        }
+        if(r>INT_MAX || r<INT_MIN) return 0;
+        return int(r);
+    }
+};
