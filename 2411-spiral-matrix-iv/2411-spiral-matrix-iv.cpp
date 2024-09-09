920feeae0445d5a08ca15da5b9c4be2b2e2876fe
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-09-10 00:09:28 +0530

    Time: 166 ms (53.94%), Space: 130.6 MB (17.37%) - LeetHub

--- 2411-spiral-matrix-iv/2411-spiral-matrix-iv.cpp
+++ 2411-spiral-matrix-iv/2411-spiral-matrix-iv.cpp
@@ -11,40 +11,16 @@
 class Solution {
 public:
     vector<vector<int>> spiralMatrix(int m, int n, ListNode* head) {
-        vector<vector<int>> matrix(m, vector<int>(n, -1));
-        
-        int topRow = 0, bottomRow = m - 1, leftColumn = 0, rightColumn = n - 1;
-        
-        while (head) {
-            // Fill top row
-            for (int col = leftColumn; col <= rightColumn && head; ++col) {
-                matrix[topRow][col] = head->val;
-                head = head->next;
-            }
-            topRow++;
-            
-            // Fill right column
-            for (int row = topRow; row <= bottomRow && head; ++row) {
-                matrix[row][rightColumn] = head->val;
-                head = head->next;
-            }
-            rightColumn--;
-            
-            // Fill bottom row
-            for (int col = rightColumn; col >= leftColumn && head; --col) {
-                matrix[bottomRow][col] = head->val;
-                head = head->next;
-            }
-            bottomRow--;
-            
-            // Fill left column
-            for (int row = bottomRow; row >= topRow && head; --row) {
-                matrix[row][leftColumn] = head->val;
-                head = head->next;
-            }
-            leftColumn++;
+        vector<vector<int>> v(m, vector<int> (n, -1));
+        int i=0, j=0, t=0, l=0, b=m-1, r=n-1; //t=top, l=left, b=bottom, r=right
+        while(head) {
+            v[i][j]=head->val;
+            head=head->next;
+            if(i == t && j < r) {j++; if(j==r) t++;}  // fill top most unfilled row 
+            else if(j == r && i < b) {i++; if(i==b) r--;} // fill right most unfilled column
+            else if(i == b && j > l) {j--; if(j==l) b--;} // fill bottom most unfilled row
+            else if(j == l && i > t) {i--; if(i==t) l++;} // fill left most unfilled column
         }
-        
-        return matrix;
+        return v;
     }
 };
