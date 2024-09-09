07b174da84e3c2dbaf19be01ff8197d6a2a1d23e
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-09-10 00:32:00 +0530

    Time: 3 ms (38.95%), Space: 8.2 MB (45.1%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.cpp
+++ 0290-word-pattern/0290-word-pattern.cpp
@@ -1,28 +1,20 @@
 class Solution {
 public:
     bool wordPattern(string pattern, string s) {
-        vector<string> v;
         string word;
         stringstream in(s);
-        while(in >> word){
-            v.push_back(word); //pushing word into vector
-        }
-        if(pattern.size() != v.size())
-            return false;
-        
-        unordered_map<char, string> p;
-        unordered_map<string, char> mp;
+        int i=0, n = pattern.size();
+        unordered_map<char, int> p;
+        unordered_map<string, int> mp;
 
-        for(int i=0; i<v.size(); i++){
-            if(p.find(pattern[i]) != p.end() && p[pattern[i]] != v[i]){ 
-                //if key is not find or the value of key is not same
-                return false;
-            } else if(mp.find(v[i]) != mp.end() && mp[v[i]] != pattern[i]){
+        //extracting the word from s and compare
+        for(word; in>>word; i++){
+            if(i==n || p[pattern[i]] != mp[word]){
                 return false;
             }
-            p[pattern[i]] = v[i]; //storing with a -> dog key->value
-            mp[v[i]] = pattern[i]; //storing with dog->a key->value
+            p[pattern[i]] = mp[word] = i+1;
         }
-        return true;
+        
+        return i==n;
     }
 };
