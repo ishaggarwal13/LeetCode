718abf8fe744aeed3b8b45da12594f8385bd6266
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-09-10 23:36:55 +0530

    Time: 37 ms (22.92%), Space: 14.5 MB (49.67%) - LeetHub

--- 0147-insertion-sort-list/0147-insertion-sort-list.cpp
+++ 0147-insertion-sort-list/0147-insertion-sort-list.cpp
@@ -11,22 +11,21 @@
 class Solution {
 public:
     ListNode* insertionSortList(ListNode* head) {
+        //O(N^2) & O(1)
         if(!head) return head;
 
-        vector<int> ans;
-        ListNode* temp = head;
-        while(temp){
-            ans.push_back(temp->val);
-            temp = temp->next;
+        ListNode* dummy = new ListNode(0); //to create sorted list
+        ListNode* curr = head;
+        while(curr){
+            ListNode* prev = dummy;
+            ListNode* next = curr->next; //storing next node to traverse the list
+            while(prev->next && prev->next->val < curr->val){
+                prev = prev->next;
+            }
+            curr->next = prev->next;
+            prev->next = curr;
+            curr = next;
         }
-
-        sort(ans.begin(), ans.end());
-        temp = head;
-        int i=0;
-        while(temp){
-            temp->val = ans[i++];
-            temp = temp->next;
-        }
-        return head;
+        return dummy->next;
     }
 };
