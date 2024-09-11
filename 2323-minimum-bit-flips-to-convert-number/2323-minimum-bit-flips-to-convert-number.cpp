bf4fc55624f17c7eb5c032f11d43f39912e5c260
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-09-11 17:20:35 +0530

    Time: 0 ms (100%), Space: 7.7 MB (44.87%) - LeetHub

--- 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.cpp
+++ 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.cpp
@@ -1,19 +1,6 @@
 class Solution {
 public:
     int minBitFlips(int start, int goal) {
-        int count = 0;
-        
-        //xor will change the bit to 1 which is different in both start and goal
-        int num = start ^ goal; 
-
-        //Now we can count the 1's bit in num by using & 1 to check last bit 
-        //and >> right shift to process the next bit
-
-        while(num){
-            count += num & 1; //if last bit is 1
-
-            num >>=1; //right shift to check next bit
-        }
-        return count;
+        return bitset<32>(start^goal).count();
     }
 };
