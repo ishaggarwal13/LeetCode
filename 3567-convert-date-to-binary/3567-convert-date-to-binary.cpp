b9f2de25e9a0f6d332494db7e3560e0c9571a668
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-09-13 00:24:57 +0530

    Time: 0 ms (100%), Space: 9.3 MB (44.46%) - LeetHub

--- 3567-convert-date-to-binary/3567-convert-date-to-binary.cpp
+++ 3567-convert-date-to-binary/3567-convert-date-to-binary.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
-    string convert(int num){
-        if(num == 1) return "1";
-        string ans = "";
-        while(num){
-            ans = to_string(num%2) + ans;
-            num /= 2;
-        }
-        return ans;
+    string convert(bitset<32>& num){
+        string s = num.to_string(); //convert bitset into binary string
+
+        int index = s.find('1'); //finding first 1 index
+
+        return s.substr(index);
     }
     string convertDateToBinary(string date) {
-        string year = convert(stoi(date.substr(0, 4)));
-        string mon = convert(stoi(date.substr(5, 2)));
-        string d = convert(stoi(date.substr(8, 2)));
+        bitset<32> year(stoi(date.substr(0, 4)));
+        bitset<32> mon(stoi(date.substr(5, 2)));
+        bitset<32> d(stoi(date.substr(8, 2)));
 
-        return year + "-" + mon + "-" + d;
+        string ans = convert(year);
+        ans += "-" + convert(mon);
+        ans += "-" + convert(d);
+
+        return ans;
     }
 };
