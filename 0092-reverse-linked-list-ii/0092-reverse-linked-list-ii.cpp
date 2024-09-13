7ac5bbc2dacd9cb4d272bc26edf32329ed960419
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-09-13 17:09:26 +0530

    Time: 2 ms (49.96%), Space: 10.3 MB (94.15%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.cpp
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.cpp
@@ -10,22 +10,36 @@
  */
 class Solution {
 public:
-    ListNode* node;
-    ListNode* reverseBetween(ListNode* head, int left, int right) {
-        if(left == 1){
-            if(left == right){
-                node = head->next;
-                return head;
-            }
-            //reversing the list
-            ListNode* nextNode = reverseBetween(head->next, left, right-1);
-            head->next->next = head;
-            head->next = node;
-            return nextNode;
+     ListNode* reverseBetween(ListNode* head, int left, int right) {
+        // Base case: if left is 1, we start reversing from here
+        if (left == 1) {
+            return reverseFirstN(head, right);
         }
-         
-        //till when we get the left = 1 which means it will start from left
-        head->next = reverseBetween(head->next, left-1, right-1);
+        
+        //move towards the left position
+        head->next = reverseBetween(head->next, left - 1, right - 1);
         return head;
+    }
+
+private:
+    ListNode* reverseFirstN(ListNode* head, int n) {
+        // Base case: if n == 1, we stop
+        if (n == 1) {
+            return head;
+        }
+
+        // Recursively reverse the next `n-1` nodes
+        ListNode* newHead = reverseFirstN(head->next, n - 1);
+
+        // Reversing the current pair of nodes
+
+        ListNode* nextNode = head->next; //nextNode is the node that will be moved to the front.
+        //Skip nextNode in the original list and link head to the node after nextNode. 
+        //This detaches nextNode.
+        head->next = nextNode->next;
+        nextNode->next = head; //Place nextNode before head to reverse their order.
+
+        // Return the new head of the reversed list
+        return newHead;
     }
 };
