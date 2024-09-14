19c8dc412ebcbe4f72c17b5a7c811135f7614bb7
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-09-14 11:22:57 +0530

    Time: 31 ms (78.99%), Space: 14.7 MB (51.96%) - LeetHub

--- 2149-remove-colored-pieces-if-both-neighbors-are-the-same-color/2149-remove-colored-pieces-if-both-neighbors-are-the-same-color.cpp
+++ 2149-remove-colored-pieces-if-both-neighbors-are-the-same-color/2149-remove-colored-pieces-if-both-neighbors-are-the-same-color.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     bool winnerOfGame(string colors) {
+        if(colors.size() == 1 || colors.size() == 2) return false;
         int alice=0, bob=0;
         
         for(int i=1; i<colors.size()-1; i++){ //not taking first and last element
