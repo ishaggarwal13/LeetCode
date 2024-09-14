7b06456dbb4c3e6b7657ff9cc92e171c564469cd
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-09-14 12:21:57 +0530

    Time: 86 ms (90.57%), Space: 84.9 MB (34.21%) - LeetHub

--- 2503-longest-subarray-with-maximum-bitwise-and/2503-longest-subarray-with-maximum-bitwise-and.cpp
+++ 2503-longest-subarray-with-maximum-bitwise-and/2503-longest-subarray-with-maximum-bitwise-and.cpp
@@ -4,16 +4,13 @@ public:
         //find maximum element
         int maxval = *max_element(nums.begin(), nums.end());
         //length of longest continous subrray with maxi ele
-        int ans=INT_MIN;
-        int i=0, j=0; //two pointers to put i at start of window and j traverse till contigous eleemnt
-        while(j<nums.size()){
-            if(nums[j] == maxval){
-                ans = max(ans, j-i+1); //end-start+1
-                j++;
-            } else {
-                j++;
-                i=j; //i will inc till element found
+        int ans=0;
+        int j=-1;
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i] != maxval){
+                j = i; //inc j value till we find the same maxval
             }
+            ans = max(ans, i - j);
         }
         return ans;
     }
