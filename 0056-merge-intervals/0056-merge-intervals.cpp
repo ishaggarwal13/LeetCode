215b2c12206adee32a0053371ab2c889cc5450b5
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-09-16 00:33:17 +0530

    Time: 21 ms (73.05%), Space: 23 MB (36.38%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,18 +1,14 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        //sort the array in asec order
+        if(intervals.size()<=1) return intervals;
         sort(intervals.begin(), intervals.end());
-        vector<vector<int>> ans;
-        for(auto interval : intervals){
-            //if arr is emoty tehn insert first vector and after that
-            //compare the last element of vector with curr first element is it is greater then add
-            if(ans.empty() || ans.back()[1] < interval[0]){
-                ans.push_back(interval);
-            } else{ //otherwise update second element in the vector
-                ans.back()[1] = max(ans.back()[1], interval[1]);
-            }
+        vector<vector<int>> output;
+        output.push_back(intervals[0]);
+        for(int i=1; i<intervals.size(); i++) {
+            if(output.back()[1] >= intervals[i][0]) output.back()[1] = max(output.back()[1] , intervals[i][1]);
+            else output.push_back(intervals[i]); 
         }
-        return ans;
+        return output;
     }
 };
