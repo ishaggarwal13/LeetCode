90dd11106a18f75857e45bc16b8d36d3797fc0b3
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-09-15 23:53:36 +0530

    Time: 6 ms (20.58%), Space: 13.3 MB (78.69%) - LeetHub

--- 2536-determine-if-two-events-have-conflict/2536-determine-if-two-events-have-conflict.cpp
+++ 2536-determine-if-two-events-have-conflict/2536-determine-if-two-events-have-conflict.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     bool haveConflict(vector<string>& event1, vector<string>& event2) {
-        return (event1[0] <= event2[0] && event1[1] >= event2[0] || event2[0] <= event1[0] && event2[1] >= event1[0]);
+        if( ( event1[0] <= event2[0] && event1[1] >= event2[0] ) 
+            || ( event2[0] <= event1[0] && event2[1] >= event1[0] ) ){
+                return true;
+            }
+        return false;
     }
 };
