8c4b667f0cf873f4233c769da22cf87580a07fa2
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-09-17 01:11:54 +0530

    Time: 2 ms (49.64%), Space: 10.5 MB (19.08%) - LeetHub

--- 1137-height-checker/1137-height-checker.cpp
+++ 1137-height-checker/1137-height-checker.cpp
@@ -1,13 +1,21 @@
 class Solution {
 public:
     int heightChecker(vector<int>& heights) {
-        vector<int> v(heights);
-        sort(v.begin(),v.end());
-        int count = 0;
-        for(int i=0;i<heights.size();i++){
-            if(heights[i] != v[i])
-             count++;
+       vector<char> sorted(heights.begin(), heights.end());
+        // counting sort
+        char freq[101]={0};
+        for(char x: sorted) freq[x]++;
+        int cnt=0;
+        for (int x=1; x<=100; x++){
+            int f=freq[x];
+            fill(sorted.begin()+cnt, sorted.begin()+cnt+f, x);
+            cnt+=f;
         }
-        return count;
+        //end of count sort
+
+        int n=heights.size(), ans=0;
+        for(int i=0; i<n; i++)
+            ans+=(heights[i]!=sorted[i]);
+        return ans;
     }
 };
