7df73c8bce81877d4d9c836ec67d9e61cb68b84d
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-09-17 00:07:15 +0530

    Time: 32 ms (34.32%), Space: 28.5 MB (73.07%) - LeetHub

--- 0495-teemo-attacking/0495-teemo-attacking.cpp
+++ 0495-teemo-attacking/0495-teemo-attacking.cpp
@@ -1,10 +1,12 @@
 class Solution {
 public:
     int findPoisonedDuration(vector<int>& timeSeries, int duration) {
+        int n = timeSeries.size();
+        if(n==0) return 0;
         int time = 0;
-        for(int i=0; i<timeSeries.size()-1; i++){ //start to end-1
-            int timediff = timeSeries[i+1] - timeSeries[i];
-            time += (timediff < duration) ? (timediff) : (duration);
+        for(int i=0; i<n-1; i++){ //start to end-1
+            if(timeSeries[i] + duration <= timeSeries[i+1]) time += duration;
+            else time += timeSeries[i+1] - timeSeries[i];
         }
         time += duration;
         return time;
