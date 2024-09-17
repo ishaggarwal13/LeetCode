be22d3beb71faa5f25b9c056a8fd63a3a2eae57f
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-09-18 00:29:02 +0530

    Time: 26 ms (40.63%), Space: 22.3 MB (97.75%) - LeetHub

--- 1028-interval-list-intersections/1028-interval-list-intersections.cpp
+++ 1028-interval-list-intersections/1028-interval-list-intersections.cpp
@@ -1,17 +1,26 @@
 class Solution {
 public:
     vector<vector<int>> intervalIntersection(vector<vector<int>>& firstList, vector<vector<int>>& secondList) {
-        vector<vector<int>> ans;
+        vector<vector<int>> result;
         // two pointer approach by taking low with maximum value and high with minimum value to get intersection
-        for(int i=0, j=0; i<firstList.size() && j<secondList.size();){
-            int low = max(firstList[i][0], secondList[j][0]);
-            int high = min(firstList[i][1], secondList[j][1]);
-            if(low <= high) {
-                ans.push_back({low, high});
+        int i = 0, j = 0;
+        while(i < firstList.size() && j < secondList.size()){
+            int s1 = firstList[i][0], e1 = firstList[i][1];
+            int s2 = secondList[j][0], e2 = secondList[j][1];
+
+            if (e1 >= s2 && e2 >= s1) {
+                int start = max(s1, s2);
+                int end = min(e1, e2);
+                result.push_back({start, end});
             }
-            if(high == firstList[i][1]) i++;
-            else j++;
+
+            if(e1 < e2){
+                i++;
+            }
+            else{
+                j++;
+            }
         }
-        return ans;
+        return result;
     }
 };
