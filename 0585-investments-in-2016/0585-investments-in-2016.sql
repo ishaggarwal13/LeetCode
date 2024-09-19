cd341bd7fbe10dd5ab90111e5bf06f388e4f4641
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-09-20 00:48:37 +0530

    Time: 691 ms (12.32%), Space: 0B (100%) - LeetHub

--- 0585-investments-in-2016/0585-investments-in-2016.sql
+++ 0585-investments-in-2016/0585-investments-in-2016.sql
@@ -1,24 +1,13 @@
 # Write your MySQL query statement below
-SELECT
-    ROUND(SUM(insurance.TIV_2016),2) AS TIV_2016
-FROM
-    insurance
-WHERE
-    insurance.TIV_2015 IN
+select
+    round(sum(tiv_2016), 2) as tiv_2016
+from
     (
-      SELECT
-        TIV_2015
-      FROM
-        insurance
-      GROUP BY TIV_2015
-      HAVING COUNT(*) > 1
-    )
-    AND CONCAT(LAT, LON) IN
-    (
-      SELECT
-        CONCAT(LAT, LON)
-      FROM
-        insurance
-      GROUP BY LAT , LON
-      HAVING COUNT(*) = 1
-    )
+        select
+            *
+            , count(*) over (partition by tiv_2015) as tiv_2015_cnt
+            , count(*) over (partition by lat, lon) as location_cnt
+        from
+            insurance
+    ) t -- every derived table must have its own alias
+where tiv_2015_cnt > 1 and location_cnt = 1
