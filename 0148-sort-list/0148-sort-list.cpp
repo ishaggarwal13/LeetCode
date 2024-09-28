e178a74840181e96d712443c58a357eb80e896ec
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-09-28 22:44:24 +0530

    Time: 119 ms (82%), Space: 58 MB (83.23%) - LeetHub

--- 0148-sort-list/0148-sort-list.cpp
+++ 0148-sort-list/0148-sort-list.cpp
@@ -10,22 +10,39 @@
  */
 class Solution {
 public:
-    ListNode* sortList(ListNode* head) {
-        vector<int> ans;
-        ListNode* temp = head;
-        while(temp){
-            ans.push_back(temp->val);
-            temp = temp->next;
+    ListNode* merge(ListNode* l1, ListNode* l2) {
+        if(!l1)
+            return l2;
+        if(!l2)
+            return l1;
+    
+        if(l1 -> val <= l2 -> val)
+        {
+            l1 -> next = merge(l1 -> next, l2);
+            return l1;
+        } else {
+            l2 -> next = merge(l1, l2 -> next);
+            return l2;
         }
-
-        sort(ans.begin(), ans.end());
-        temp = head;
-        int i=0;
-        while(temp){
-            temp->val = ans[i++];
-            temp = temp->next;
+    }
+    
+    ListNode* sortList(ListNode* head) 
+    {
+        if(head == NULL || head -> next == NULL)
+            return head;
+    
+        ListNode* slow = head;
+        ListNode* fast = head -> next;  // Finding the middle point first
+    
+        while(fast && fast -> next)
+        {
+            slow = slow -> next;
+            fast = fast -> next -> next;
         }
-
-        return head;
+    
+        ListNode* newHead = slow -> next;  // creating two seperate LL 
+        slow -> next = NULL;   
+    
+        return merge(sortList(head), sortList(newHead));
     }
 };
