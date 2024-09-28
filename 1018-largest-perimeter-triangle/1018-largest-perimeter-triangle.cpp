2ac46a6a6a655301e8d3265fcd4690fa30a675c9
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-09-28 22:57:01 +0530

    Time: 40 ms (8.21%), Space: 28 MB (8.63%) - LeetHub

--- 1018-largest-perimeter-triangle/1018-largest-perimeter-triangle.cpp
+++ 1018-largest-perimeter-triangle/1018-largest-perimeter-triangle.cpp
@@ -2,15 +2,13 @@ class Solution {
 public:
     int largestPerimeter(vector<int>& nums) {
         // sort side length in descending order
-        std::sort( nums.rbegin(), nums.rend() );
+        sort( nums.rbegin(), nums.rend() );
         
-        for( int i = 0 ; i < nums.size()-2 ; i++ ){
-            
+        for( int i = 0 ; i < nums.size()-2 ; i++ ){        
             // Accept: find the triangle with largest perimeter
             if( nums[i] < ( nums[i+1] + nums[i+2]) ){
                 return nums[i] + nums[i+1] + nums[i+2];
-            }
-            
+            } 
         }
         
         // Reject: impossible to make triangle
