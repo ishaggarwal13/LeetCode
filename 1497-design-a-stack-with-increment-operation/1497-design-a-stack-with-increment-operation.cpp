ebd0ddab72390041a64b48348b411de6397cddcd
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-09-30 20:57:30 +0530

    Time: 55 ms (5.19%), Space: 52.7 MB (5.36%) - LeetHub

--- 1497-design-a-stack-with-increment-operation/1497-design-a-stack-with-increment-operation.cpp
+++ 1497-design-a-stack-with-increment-operation/1497-design-a-stack-with-increment-operation.cpp
@@ -1,34 +1,62 @@
 class CustomStack {
 public:
-    int n;
-    stack<int> stack;
-    vector<int> inc;
-
-    CustomStack(int n) {
-        this->n = n;
+    queue<int> q;
+    int mx;
+    
+public:
+    CustomStack(int maxSize) {
+        mx = maxSize;
     }
-
+    
     void push(int x) {
-        if (stack.size() < n) {
-            stack.push(x);
-            inc.push_back(0);
-        }
+        if (q.size() == mx) return;
+        q.push(x);
     }
-
+    
     int pop() {
-        if (stack.empty()) return -1;
-        if (inc.size() > 1) inc[inc.size() - 2] += inc.back();
-        int res = stack.top() + inc.back();
-        stack.pop();
-        inc.pop_back();
-        return res;
-    }
-
-    void increment(int k, int val) {
-        if (!stack.empty()) {
-            int idx = min(k, (int)inc.size()) - 1;
-            inc[idx] += val;
+        if (q.empty()) return -1;  // Return -1 if the stack is empty
+        
+        int n = q.size() - 1;
+        queue<int> tmp_q;
+        
+        // Move all elements except the last one to a temporary queue
+        for (int i = 0; i < n; i++) {
+            tmp_q.push(q.front());
+            q.pop();
         }
+        
+        // This is the element to be removed (top of the stack)
+        int poppedElement = q.front();
+        q.pop();
+        
+        // Restore the elements back into the original queue
+        while (!tmp_q.empty()) {
+            q.push(tmp_q.front());
+            tmp_q.pop();
+        }
+        
+        return poppedElement;  // Return the popped element
+    }
+    
+    void increment(int k, int val) {
+        int n = min(k, (int)q.size());  // Increment only the first k elements
+        queue<int> tmp_q;
+        
+        // Increment the first k elements
+        for (int i = 0; i < n; i++) {
+            int incrementedValue = q.front() + val;
+            tmp_q.push(incrementedValue);
+            q.pop();
+        }
+        
+        // Move the rest of the elements as they are
+        while (!q.empty()) {
+            tmp_q.push(q.front());
+            q.pop();
+        }
+        
+        // Restore the elements back into the original queue
+        q = tmp_q;
     }
 };
 
