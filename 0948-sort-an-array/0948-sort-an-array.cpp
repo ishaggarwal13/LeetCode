c579854ee41b2cbbf766f56dfb87eb0ccac8fc3d
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-10-02 21:24:54 +0530

    Time: 113 ms (73.86%), Space: 69.3 MB (94.04%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.cpp
+++ 0948-sort-an-array/0948-sort-an-array.cpp
@@ -1,44 +1,29 @@
-void merge(vector<int>& array, int low, int mid, int high) {
-    int n1 = mid - low + 1;
-    int n2 = high - mid;
-    vector<int> left_part(n1), right_part(n2);
-
-    for (int i = 0; i < n1; ++i)
-        left_part[i] = array[low + i];
-    for (int i = 0; i < n2; ++i)
-        right_part[i] = array[mid + 1 + i];
-
-    int p1 = 0, p2 = 0, write_ind = low;
-    while (p1 < n1 && p2 < n2) {
-        if (left_part[p1] <= right_part[p2]) {
-            array[write_ind] = left_part[p1++];
-        } else {
-            array[write_ind] = right_part[p2++];
-        }
-        ++write_ind;
-    }
-
-    while (p1 < n1)
-        array[write_ind++] = left_part[p1++];
-
-    while (p2 < n2)
-        array[write_ind++] = right_part[p2++];
-}
-
-void merge_sort(vector<int>& array, int low, int high) {
-    if (low >= high)
-        return;
-
-    int mid = low + (high - low) / 2;
-    merge_sort(array, low, mid);
-    merge_sort(array, mid + 1, high);
-    merge(array, low, mid, high);
-}
-
 class Solution {
 public:
+    void maxHeapify(vector<int>& nums, int n, int i){
+        int largest = i;
+        int left = (2 * i) + 1, right = (2 * i) + 2;
+        if(left < n && nums[left] > nums[largest])
+            largest = left;
+        if(right < n && nums[right] > nums[largest])
+            largest = right;
+        if(largest != i){
+            swap(nums[largest], nums[i]);
+            maxHeapify(nums, n, largest);
+        }
+    }
+
+    void heapSort(vector<int>& nums, int n){
+        for(int i = n/2-1; i >= 0; i--)
+            maxHeapify(nums, n, i);
+        for(int i = n-1; i >= 0; i--){
+            swap(nums[0], nums[i]);
+            maxHeapify(nums, i, 0);
+        }
+    }
+
     vector<int> sortArray(vector<int>& nums) {
-        merge_sort(nums, 0, nums.size() - 1);
+        heapSort(nums, nums.size());
         return nums;
     }
 };
