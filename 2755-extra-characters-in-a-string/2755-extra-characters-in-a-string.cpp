2ce71c132b4b32661f72215e428bb9d3a7e5d72b
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-10-02 21:35:32 +0530

    Time: 126 ms (59.23%), Space: 86.7 MB (56.34%) - LeetHub

--- 2755-extra-characters-in-a-string/2755-extra-characters-in-a-string.cpp
+++ 2755-extra-characters-in-a-string/2755-extra-characters-in-a-string.cpp
@@ -1,25 +1,20 @@
 class Solution {
 public:
     int minExtraChar(const string &s, vector<string>& dictionary) {
-        int const n = s.size();
-        std::vector<int> dp(n+1);
+        int max_val = s.length() + 1;
+        vector<int> dp(s.length() + 1, max_val);
+        dp[0] = 0;
 
-        for(int i = 1; i < n+1; ++i) {
-            dp[i] = dp[i-1] + 1;
-            for(std::string const &word: dictionary) {
-                int const m = word.length();
-                if(i >= m) {
-                    bool flag = true;
-                    for(int j = i-m; j < i; ++j) {
-                        if(s[j] != word[j-i+m]) {
-                            flag = false;
-                            break;
-                        }
-                    }
-                    if(flag) { dp[i] = std::min(dp[i], dp[i-m]); }
+        unordered_set<string> dictionary_set(dictionary.begin(), dictionary.end());
+
+        for (int i = 1; i <= s.length(); ++i) {
+            dp[i] = dp[i - 1] + 1;
+            for (int l = 1; l <= i; ++l) {
+                if (dictionary_set.find(s.substr(i - l, l)) != dictionary_set.end()) {
+                    dp[i] = min(dp[i], dp[i - l]);
                 }
             }
         }
-        return dp[n];
+        return dp.back();
     }
 };
