3f2599da8deac3db84832697e83d4712f904212b
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-10-04 23:08:35 +0530

    Time: 23 ms (70.8%), Space: 30.7 MB (69.44%) - LeetHub

--- 1484-linked-list-in-binary-tree/1484-linked-list-in-binary-tree.cpp
+++ 1484-linked-list-in-binary-tree/1484-linked-list-in-binary-tree.cpp
@@ -22,21 +22,14 @@
 class Solution {
 public:
     bool isSubPath(ListNode* head, TreeNode* root) {
-        return dfs(head, head, root);
+        if(root == NULL) return false;
+        return find(head,root) || isSubPath(head,root->left) || isSubPath(head,root->right);
     }
-    
-    bool dfs(ListNode* head, ListNode* cur, TreeNode* root) {
-        if (cur == nullptr) return true;  // Successfully matched the list
-        if (root == nullptr) return false; // Reached the end of the tree without matching
-        
-        if (cur->val == root->val) {
-            cur = cur->next;  // Move to the next list node if value matches
-        } else if (head->val == root->val) {
-            head = head->next; // Start new matching attempt if the value matches head of list
-        } else {
-            cur = head;  // Reset the matching pointer
-        }
-        
-        return dfs(head, cur, root->left) || dfs(head, cur, root->right);
+    bool find(ListNode* head, TreeNode* root){
+        if(head == NULL) return true;
+        if(root == NULL) return false;
+        if(root->val == head->val)
+            return find(head->next,root->left) || find(head->next,root->right);
+        else return false;
     }
 };
