a340aaf8dca3b6e4c121b12595789c6fb4d1454d
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-10-05 23:36:57 +0530

    Time: 15 ms (30.44%), Space: 10.8 MB (25.36%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        if (s1.length() > s2.length()) return false;
-
-        vector<int> s1Count(26, 0), s2Count(26, 0);
+        int n1 = s1.size(), n2 = s2.size();
         
-        // Count frequencies of s1 and the first window in s2
-        for (int i = 0; i < s1.length(); ++i) {
-            s1Count[s1[i] - 'a']++;
-            s2Count[s2[i] - 'a']++;
+        if (n1 > n2) return false;
+
+        unordered_map<char, int> ctr1, ctr2;
+
+        for (int i = 0; i < n1; ++i) {
+            ctr1[s1[i]]++;
+            ctr2[s2[i]]++;
         }
 
-        // Slide the window over s2
-        for (int i = 0; i < s2.length() - s1.length(); ++i) {
-            if (s1Count == s2Count) return true;
-            // Update the window
-            s2Count[s2[i] - 'a']--;
-            s2Count[s2[i + s1.length()] - 'a']++;
+        for (int i = n1; i < n2; ++i) {
+            if (ctr1 == ctr2) return true;
+            ctr2[s2[i - n1]]--;  
+            if (ctr2[s2[i - n1]] == 0) ctr2.erase(s2[i - n1]); 
+            ctr2[s2[i]]++;
         }
-
-        // Check the last window
-        return s1Count == s2Count;
+        return ctr1 == ctr2;
     }
 };
