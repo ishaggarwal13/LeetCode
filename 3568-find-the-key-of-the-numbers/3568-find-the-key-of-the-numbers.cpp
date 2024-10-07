bb4faf25e508083c7a4a17edf645acb70445055e
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-10-07 17:31:00 +0530

    Time: 0 ms (100%), Space: 10.6 MB (7.71%) - LeetHub

--- 3568-find-the-key-of-the-numbers/3568-find-the-key-of-the-numbers.cpp
+++ 3568-find-the-key-of-the-numbers/3568-find-the-key-of-the-numbers.cpp
@@ -1,20 +1,27 @@
 class Solution {
 public:
     int generateKey(int num1, int num2, int num3) {
-        //string conversion
-        string s = "0000", res = "";
-        string s1 = to_string(num1);
-        string s2 = to_string(num2); 
-        string s3 = to_string(num3);
+        string a1 = to_string(num1);
+        string a2 = to_string(num2);
+        string a3 = to_string(num3);
 
-        s1 = s.substr(0, s.size()-s1.size()) + s1;
-        s2 = s.substr(0, s.size()-s2.size()) + s2;
-        s3 = s.substr(0, s.size()-s3.size()) + s3;
-
-        for(int i=0; i<s.size(); i++){
-            res += min(s1[i], min(s2[i], s3[i]));
+        vector<char> out;
+        vector<string> ans(3);
+        ans[0] = a1;
+        ans[1] = a2;
+        ans[2] = a3;
+        for (int i = 0; i < ans.size(); i++) {
+            while (ans[i].size() < 4) {
+                ans[i] = '0' + ans[i];
+            }
         }
-        
-        return stoi(res); //string to int conversion
+        int j = 0;
+        while (j < 4) {
+            char u = min({ans[0][j], ans[1][j], ans[2][j]});
+            out.push_back(u);
+            j++;
+        }
+        string key(out.begin(), out.end());
+        return stoi(key);
     }
 };
