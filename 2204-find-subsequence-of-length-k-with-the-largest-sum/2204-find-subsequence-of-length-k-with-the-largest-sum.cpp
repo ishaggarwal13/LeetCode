bb9972c1f44fd458cd9992a925e598ec6455941f
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-10-08 23:01:24 +0530

    Time: 7 ms (41.25%), Space: 12.3 MB (91.32%) - LeetHub

--- 2204-find-subsequence-of-length-k-with-the-largest-sum/2204-find-subsequence-of-length-k-with-the-largest-sum.cpp
+++ 2204-find-subsequence-of-length-k-with-the-largest-sum/2204-find-subsequence-of-length-k-with-the-largest-sum.cpp
@@ -1,27 +1,21 @@
 class Solution {
 public:
     vector<int> maxSubsequence(vector<int>& nums, int k) {
-        int n = nums.size();
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-        for (int i=0; i<n; i++) {
-            if (pq.size() < k) {
-                pq.push({nums[i], i});
-            } else if (pq.top().first < nums[i]) {
-                pq.pop();
-                pq.push({nums[i], i});
+        vector<int> a = nums;
+        sort(nums.begin(),nums.end());
+        reverse(nums.begin(),nums.end());
+        
+        vector<int> ans;
+        map<int,int> mpp;
+        for(int i=0;i<k;i++){
+            mpp[nums[i]]++;
+        }
+        for(int i=0;i<a.size();i++){
+            if(mpp.find(a[i]) != mpp.end() && mpp[a[i]] !=0){
+                ans.push_back(a[i]);
+                mpp[a[i]]--;
             }
         }
-
-        vector<int> mark(n, 0), res;
-        while (!pq.empty()) {
-            mark[pq.top().second] = 1;
-            pq.pop();
-        }
-        for (int i=0; i<n; i++) {
-            if (mark[i] == 1) {
-                res.push_back(nums[i]);
-            }
-        }
-        return res;
+        return ans;
     }
 };
