11440178662828fd08a35a4e72feb82abd757307
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-10-09 11:46:52 +0530

    Time: 134 ms (17.06%), Space: 44.2 MB (5.57%) - LeetHub

--- 2095-minimum-number-of-swaps-to-make-the-string-balanced/2095-minimum-number-of-swaps-to-make-the-string-balanced.cpp
+++ 2095-minimum-number-of-swaps-to-make-the-string-balanced/2095-minimum-number-of-swaps-to-make-the-string-balanced.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int minSwaps(string s) {
-        stack<char> st;
-        
-        for(char &ch : s) {
-            if(ch == '[')        st.push(ch);
+        stack<int> st;
+
+        for(auto c : s){
+            if(c == '[') st.push(c);
             else if(!st.empty()) st.pop();
-        }       
+        }
         return (st.size()+1)/2;
     }
 };
