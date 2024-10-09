6d06adf08adf5a6a379fbb903fd0937057e3fc23
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-10-09 11:24:08 +0530

    Time: 8 ms (44.66%), Space: 12.1 MB (11.38%) - LeetHub

--- 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.cpp
+++ 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.cpp
@@ -1,30 +1,14 @@
 class Solution {
 public:
     int minLength(string s) {
-        if(s.length() == 1) return 1;
-
-        stack<char> st;
-        for(auto c : s){
-            if(!st.empty()){
-                if(c == 'B' && st.top() == 'A'){
+        stack<int> st;
+        for(char ch : s){
+            if(!st.empty() && ((st.top() == 'A' && ch == 'B') || (st.top() == 'C' && ch == 'D'))){
                 st.pop();
-                continue;
+            } else {
+                st.push(ch);
             }
-            else if(c == 'D' && st.top() == 'C'){
-                st.pop();
-                continue;
-            }
-            }
-                st.push(c);
         }
-
-        string t = "";
-        while(!st.empty()){
-            char x = st.top();
-            t += x;
-            st.pop();
-        }
-
-        return t.length();
+        return st.size();
     }
 };
