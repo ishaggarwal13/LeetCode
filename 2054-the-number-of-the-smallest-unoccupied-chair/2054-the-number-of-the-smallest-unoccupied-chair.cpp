457043471a61950e6d4056a1082d302d61d67554
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-10-11 16:07:17 +0530

    Time: 318 ms (13.4%), Space: 58.5 MB (81.79%) - LeetHub

--- 2054-the-number-of-the-smallest-unoccupied-chair/2054-the-number-of-the-smallest-unoccupied-chair.cpp
+++ 2054-the-number-of-the-smallest-unoccupied-chair/2054-the-number-of-the-smallest-unoccupied-chair.cpp
@@ -2,40 +2,19 @@ class Solution {
 public:
     int smallestChair(vector<vector<int>>& times, int targetFriend) {
         int n = times.size();
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>> > busy; //departure time as first , index as second
-        priority_queue<int, vector<int>, greater<int>> free; //min heap of chair index that are unoccupied
-        
-		//store friend indexes so that they don't get lost after sorting
-        for(int i=0;i<n;++i){
-            times[i].push_back(i);
-        }
-		//Sort according to arrival time
-        sort(times.begin(),times.end());
-        
-		int new_chair = 0; //chairs alloted till now 
-        for(int i=0;i<n;++i){
-            int arrival = times[i][0]; //pop chairs before arrival 
-            int leave_time = times[i][1];
-            int fr = times[i][2]; //friend index
-            //free chairs accordingly
-            while(!busy.empty() && busy.top().first <= arrival){
-                // cout << "Chair free " << busy.top().second << endl;
-                free.push(busy.top().second);
-                busy.pop();
-            }   
-            //No free chair allot new chair
-            if(free.empty()){
-                // cout << "Alloting new_chair " << new_chair << "to" << fr << endl;
-                if(fr  == targetFriend) return new_chair;
-                busy.push({leave_time,new_chair});
-                new_chair++;
-            }
-            else{
-                int x = free.top();
-                // cout << "giving chair " << x << "to" << fr << endl;
-                free.pop();
-                if( fr  == targetFriend) return x; 
-                busy.push({leave_time,x});
+        vector<int> chairs(n, -1);
+        int tar = times[targetFriend][0];
+        sort(times.begin(), times.end());
+        for (auto it : times) {
+            int arr = it[0];
+            int dep = it[1];
+            for (int i = 0; i < n; i++) {
+                if (arr >= chairs[i]) {
+                    chairs[i] = dep;
+                    if (arr == tar)
+                        return i;
+                    break;
+                }
             }
         }
         return -1;
