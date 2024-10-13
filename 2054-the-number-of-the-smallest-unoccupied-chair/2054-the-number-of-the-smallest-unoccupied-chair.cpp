cec0d2df120bafe4ef870c7cdffb0569a99fe63d
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-10-13 23:44:44 +0530

    Time: 121 ms (90.16%), Space: 55 MB (96%) - LeetHub

--- 2054-the-number-of-the-smallest-unoccupied-chair/2054-the-number-of-the-smallest-unoccupied-chair.cpp
+++ 2054-the-number-of-the-smallest-unoccupied-chair/2054-the-number-of-the-smallest-unoccupied-chair.cpp
@@ -2,19 +2,38 @@ class Solution {
 public:
     int smallestChair(vector<vector<int>>& times, int targetFriend) {
         int n = times.size();
-        vector<int> chairs(n, -1);
+        priority_queue<pair<int, int>, vector<pair<int, int>>,
+                       greater<pair<int, int>>>
+            occChair;
+        priority_queue<int, vector<int>, greater<int>> freeChair;
+
         int tar = times[targetFriend][0];
         sort(times.begin(), times.end());
-        for (auto it : times) {
-            int arr = it[0];
-            int dep = it[1];
-            for (int i = 0; i < n; i++) {
-                if (arr >= chairs[i]) {
-                    chairs[i] = dep;
-                    if (arr == tar)
-                        return i;
-                    break;
+
+        int chair = 0;
+
+        for (int i = 0; i < n; i++) {
+            int arr = times[i][0];
+            int dep = times[i][1];
+
+            while (!occChair.empty() && occChair.top().first <= arr) {
+                freeChair.push(occChair.top().second);
+                occChair.pop();
+            }
+
+            if (freeChair.empty()) {
+                occChair.push({dep, chair});
+                if (arr == tar) {
+                    return chair;
                 }
+                chair++;
+            } else {
+                int minChair = freeChair.top();
+                freeChair.pop();
+                if (arr == tar) {
+                    return minChair;
+                }
+                occChair.push({dep, minChair});
             }
         }
         return -1;
