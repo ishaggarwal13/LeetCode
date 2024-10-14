9c09dd9ddce909cfc9588b2d40f9ba74b5493d50
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-10-14 23:01:54 +0530

    Time: 86 ms (13.3%), Space: 27.3 MB (12.64%) - LeetHub

--- 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.cpp
+++ 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.cpp
@@ -1,86 +1,51 @@
 class Solution {
-private:
-    bool chk(int a,int b, int c, int d){
-        // if new numbers are answer
-        if(b-a < d-c){
-            return true;
-        }
-        else if(b-a == d-c){
-            if(a<c){return true;}
-        }
-
-        // new number dont have minimum difference
-        // uptill now
-        return false;
-    }
 public:
-    vector<int> smallestRange(vector<vector<int>>& a) {
-        int ansx = -1e5, ansy = 1e5; 
-        int total_list = a.size();
+    vector<int> smallestRange(vector<vector<int>>& nums) {
+        // Min heap for getting the minimum element value of the range
+        // Custom comparator: compare based on the first element in the vector
+        auto comp = [](const vector<int>& a, const vector<int>& b) {
+            return a[0] > b[0]; // this ensures we get the smallest element first (min-heap)
+        };
 
-        // UNCOMMENT TO CHECK HOW GIVEN ARRAY PRINT AFTER SORT
-        // sort(a.begin(), a.end());
-        //     cout<<"[";
-        // for(int i=0;i<total_list;i++){
-        //     cout<<"[";
-        //     for(int j=0;j<a[i].size();j++){
-        //         cout<<a[i][j]<<((j==a[i].size()-1)?"":",");
-        //     }
-        //     cout<<"]"<<((i==total_list-1)?"":",");
-        // }
-        //     cout<<"]";
-        // cout<<endl;
-        // cout<<endl;
+        priority_queue<vector<int>, vector<vector<int>>, decltype(comp)> pq(comp);
 
-        vector<pair<int,int>> vp;
-        for(int i=0;i<total_list;i++){
-            for(int j=0;j<a[i].size();j++){
-                vp.push_back({a[i][j],i});
-            }
+        int k = nums.size(); // number of lists
+        int maxval = INT_MIN; // for tracking the maximum value of the current range
+
+        // Pushing the first element of each list into the min-heap
+        for(int i = 0; i < k; i++){
+            pq.push({nums[i][0], i, 0}); // push the first element of each list with its list number and index
+            maxval = max(maxval, nums[i][0]); // update the maximum value
         }
 
+        int start = 0, end = INT_MAX; // to store the smallest range
 
-        sort(vp.begin(), vp.end());
+        // Processing the heap
+        while(!pq.empty()){
+            auto curr = pq.top(); // store the top of the min-heap (smallest element)
+            pq.pop(); // remove it from the heap
+            int minval = curr[0]; // the smallest value
+            int row = curr[1]; // list (row) it came from
+            int index = curr[2]; // the index within that list
 
-        // UNCOMMENT TO CHECK HOW VECTOR IS PRINTING AFTER SORT
-        // int cnt=0;
-        // cout<<"[";
-        // for(auto& val:vp){
-        //     cout<<"["<<val.first<<","<<val.second<<"]";
-        //     cnt++;
-        //     if(cnt!=vp.size()){cout<<",";}
-        // }
-        // cout<<"]";
-
-        unordered_map<int,int> um;
-        int l=0, r=0;
-        int n = vp.size();
-        while(r<n){
-            int num = vp[r].first;
-            int list = vp[r].second;
-
-            um[list]++;
-
-            while(um.size() >= total_list){
-                //    |-----new range---------| |--old range--|
-                if(chk(vp[l].first,vp[r].first,   ansx, ansy )){
-                    // cout<<"Possible ans = "<<ansx<<" "<<ansy<<endl;
-                    ansx = vp[l].first;
-                    ansy = vp[r].first;
-                    // cout<<"Possible ans = "<<ansx<<" "<<ansy<<endl;
-                }
-
-                int old_list = vp[l].second;
-                
-                um[old_list]--;
-                l++;
-
-                if(um[old_list]==0){um.erase(old_list);}
+            // Update the range if the current range is smaller than the previous one
+            if(maxval - minval < end - start){
+                start = minval;
+                end = maxval;
             }
 
-            r++;
-        }
+            // If the current row is exhausted, we cannot move further
+            if(index + 1 == nums[row].size()){
+                break;
+            }
 
-        return {ansx, ansy};
+            // Push the next element from the same row into the heap
+            int nextval = nums[row][index + 1];
+            pq.push({nextval, row, index + 1});
+
+            // Update the maximum value if necessary
+            maxval = max(maxval, nextval);
+        }
+        return {start, end}; // return the smallest range found
     }
 };
