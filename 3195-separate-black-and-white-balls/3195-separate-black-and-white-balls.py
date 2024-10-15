ef2a6c15d4e1d1d67e43eafdf6008f338fd316b2
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-10-15 23:24:25 +0530

    Time: 79 ms (87.76%), Space: 17.7 MB (31.34%) - LeetHub

--- 3195-separate-black-and-white-balls/3195-separate-black-and-white-balls.py
+++ 3195-separate-black-and-white-balls/3195-separate-black-and-white-balls.py
@@ -1,15 +1,10 @@
-class Solution(object):
-    def minimumSteps(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        black = 0
-        swap = 0
+class Solution:
+    def minimumSteps(self, s: str) -> int:
+        black: int = 0
+        swap: int = 0
         for i in s:
             if i == '1':
                 black += 1
             else:
                 swap += black
         return swap
-        
