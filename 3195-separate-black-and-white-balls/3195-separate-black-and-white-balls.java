04e0570b249b6f3f1750112dda49e3f2f445dc24
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-10-15 23:30:36 +0530

    Time: 7 ms (100%), Space: 45.3 MB (54.12%) - LeetHub

--- 3195-separate-black-and-white-balls/3195-separate-black-and-white-balls.java
+++ 3195-separate-black-and-white-balls/3195-separate-black-and-white-balls.java
@@ -2,9 +2,9 @@ class Solution {
     public long minimumSteps(String s) {
         int black = 0;
         long swap = 0;
-        // convert string into char of array
-        for(char c : s.toCharArray()){
-            if(c == '1'){
+        // convert string to char
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i) == '1'){
                 black++;
             } else {
                 swap += black;
