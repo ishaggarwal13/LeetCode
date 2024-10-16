24736fc5dea32eeb31fe2baaf6a77e93d23136fb
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-10-17 00:11:01 +0530

    Time: 30 ms (21.51%), Space: 30.8 MB (69.48%) - LeetHub

--- 1484-linked-list-in-binary-tree/1484-linked-list-in-binary-tree.cpp
+++ 1484-linked-list-in-binary-tree/1484-linked-list-in-binary-tree.cpp
@@ -22,14 +22,17 @@
 class Solution {
 public:
     bool isSubPath(ListNode* head, TreeNode* root) {
-        if(root == NULL) return false;
-        return find(head,root) || isSubPath(head,root->left) || isSubPath(head,root->right);
+        if(!root) return false;
+
+        if(dfs(head,root)) return true;
+
+        return isSubPath(head,root->left) || isSubPath(head,root->right);
     }
-    bool find(ListNode* head, TreeNode* root){
-        if(head == NULL) return true;
-        if(root == NULL) return false;
-        if(root->val == head->val)
-            return find(head->next,root->left) || find(head->next,root->right);
-        else return false;
+    bool dfs(ListNode* head, TreeNode* root){
+        if(!head) return true;
+
+        if(!root || head->val != root->val) return false;
+
+        return dfs(head->next,root->left) || dfs(head->next,root->right);
     }
 };
