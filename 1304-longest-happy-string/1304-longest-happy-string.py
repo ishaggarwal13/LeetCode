540823f36585990a234fc6a2dfb6d81f1ce4d170
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-10-16 22:47:26 +0530

    Time: 43 ms (8.21%), Space: 16.5 MB (94.09%) - LeetHub

--- 1304-longest-happy-string/1304-longest-happy-string.py
+++ 1304-longest-happy-string/1304-longest-happy-string.py
@@ -3,28 +3,31 @@ class Solution:
         res = "" 
         pq = []
 
-        if a>0: heapq.heappush(pq, (-a, 'a')) #storing neg value to make max-heap
-        if b>0: heapq.heappush(pq, (-b, 'b'))
-        if c>0: heapq.heappush(pq, (-c, 'c'))
+        if a>0: heappush(pq, (-a, 'a')) #storing neg value to make max-heap
+        if b>0: heappush(pq, (-b, 'b'))
+        if c>0: heappush(pq, (-c, 'c'))
 
         while pq:
             count1, char1 = heapq.heappop(pq)
+            count1 *= -1
 
-            if len(res) >= 2 and res[-1] == char1 and res[-2] == char1:
-                if not pq: break
+            if len(res) >= 2 and res[-1] == res[-2] == char1:
+                if not pq: return res
 
                 count2, char2 = heapq.heappop(pq)
+                count2 *= -1
                 res += char2
-                count2 += 1 #as we had stroed neg values
+                count2 -= 1 
 
-                if count2 < 0: heapq.heappush(pq, (count2, char2))
+                if count2: heapq.heappush(pq, (-1*count2, char2))
 
-                heapq.heappush(pq, (count1, char1))
-            else: 
-                res += char1
-                count1 += 1
+                heapq.heappush(pq, (-1*count1, char1))
+                continue
+            
+            res += char1
+            count1 -= 1
 
-                if count1 < 0: heapq.heappush(pq, (count1, char1))
+            if count1: heapq.heappush(pq, (-1*count1, char1))
         
         return res
 
