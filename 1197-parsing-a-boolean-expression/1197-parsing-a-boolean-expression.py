5e3300560eeb226e3e967eddebfdcd4b038b9292
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-10-20 19:21:32 +0530

    Time: 23 ms (99.4%), Space: 16.9 MB (10.78%) - LeetHub

--- 1197-parsing-a-boolean-expression/1197-parsing-a-boolean-expression.py
+++ 1197-parsing-a-boolean-expression/1197-parsing-a-boolean-expression.py
@@ -1,25 +1,33 @@
 class Solution:
     def parseBoolExpr(self, expression: str) -> bool:
-        st = deque()
-        for c in expression:
-            if c == "," or c == "(":
-                continue
-            if c in ["t", "f", "!", "&", "|"]:
-                st.append(c)
-            elif c == ")":
-                has_true = False
-                has_false = False
-                while st[-1] not in ["!", "&", "|"]:
-                    top_value = st.pop()
-                    if top_value == "t":
-                        has_true = True
-                    elif top_value == "f":
-                        has_false = True
-                op = st.pop()
-                if op == "!":
-                    st.append("t" if not has_true else "f")
-                elif op == "&":
-                    st.append("f" if has_false else "t")
-                else:
-                    st.append("t" if has_true else "f")
-        return st[-1] == "t"
+        operands=deque()
+        operators=deque()
+        for i in expression:
+            if i=='&' or i=='|' or i=='!':
+                operators.append(i)
+            elif i==')':
+                operator=operators.pop()
+                s=''
+                while operands[-1]!='(':
+                    s+=operands.pop()
+                    print(s)
+                operands.pop()
+                if operator=='&':
+                    if 'f' in s:
+                        operands.append('f')
+                    else:
+                        operands.append('t')
+                elif operator=='|':
+                    if 't' in s:
+                        operands.append('t')
+                    else:
+                        operands.append('f')
+                elif operator=='!':
+                    if s=='t':
+                        operands.append('f')
+                    else:
+                        operands.append('t')
+            elif i=='t' or i=='f' or i=='(':
+                operands.append(i)
+        print(operators,operands)
+        return True if operands[0]=='t' else False
