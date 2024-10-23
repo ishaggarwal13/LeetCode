d1c2f6a243b0e4e69c19c97e2d0def88d41aebec
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-10-23 20:22:05 +0530

    Time: 54 ms (94.65%), Space: 332.1 MB (21.6%) - LeetHub

---
+++ 2677-cousins-in-binary-tree-ii/2677-cousins-in-binary-tree-ii.cpp
@@ -0,0 +1,54 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    // find sum of nodes on each level
+    // then del the siblings nodes from the sum and keep the cousins nodes
+    TreeNode* replaceValueInTree(TreeNode* root) {
+        // set root value to 0 as there is no sbiling nodes to root
+        root->val = 0;
+        // start bfs or dfs anything to find the sum of level
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while(!q.empty()){
+            int level = q.size();
+            int sum = 0;
+            vector<TreeNode*> buff;
+
+            for(int i=0; i<level; i++){
+                TreeNode* curr = q.front();
+                q.pop();
+                buff.push_back(curr);
+
+                if(curr->left) {
+                    q.push(curr->left);
+                    sum += curr->left->val;
+                }
+
+                if(curr->right) {
+                    q.push(curr->right);
+                    sum += curr->right->val;
+                }
+            }
+
+            for(auto node : buff){
+                int t = sum;
+                if(node->left) t -= node->left->val;
+                if(node->right) t -= node->right->val;
+                if(node->left) node->left->val = t;
+                if(node->right) node->right->val = t;
+            }
+        }
+        return root;
+    }
+};
