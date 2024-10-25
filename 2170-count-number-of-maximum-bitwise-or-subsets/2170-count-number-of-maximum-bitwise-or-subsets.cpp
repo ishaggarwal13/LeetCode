f4b26ef807d0f93561c64da697839ce9b48bf750
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-10-25 23:53:01 +0530

    Time: 11 ms (64.54%), Space: 25.4 MB (20.18%) - LeetHub

--- 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.cpp
@@ -1,18 +1,26 @@
 class Solution {
 public:
-    int countSubsetsTarget(int idx,int curr,int const &target,vector<int> const &nums){
-        if(idx == nums.size()){
-            if(target == curr)
-                return 1;
-            return 0;
-        }
-        return (countSubsetsTarget(idx+1,(curr | nums[idx]),target,nums) + countSubsetsTarget(idx+1,curr,target,nums));
-    }
-
     int countMaxOrSubsets(vector<int>& nums) {
-        int target = 0;
-        for(auto &x:nums)
-            target |= x;
-        return countSubsetsTarget(0,0,target,nums);
+
+        int maximum = std::accumulate(nums.begin(), nums.end(), 0,
+                                      [](int a, int b) { return a | b; });
+
+        std::vector<int> ors = {0,nums[0]};
+        int also_max_subset = 0;
+
+        if(nums[0] == maximum) also_max_subset++;
+
+        for (int j = 1; j < nums.size(); ++j) {
+            int ors_size = ors.size();
+
+            for (int i = 0; i < ors_size; ++i) {
+                int value = ors[i] | nums[j];
+                if (value == maximum) 
+                    also_max_subset++;
+                ors.push_back(value);
+            }
+        }
+
+        return also_max_subset;
     }
 };
