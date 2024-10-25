40512f537a60b12418a7eb84039b0343386546b9
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-10-25 23:54:56 +0530

    Time: 71 ms (34.81%), Space: 10.1 MB (46.64%) - LeetHub

--- 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.cpp
@@ -1,26 +1,29 @@
 class Solution {
 public:
     int countMaxOrSubsets(vector<int>& nums) {
-
-        int maximum = std::accumulate(nums.begin(), nums.end(), 0,
-                                      [](int a, int b) { return a | b; });
-
-        std::vector<int> ors = {0,nums[0]};
-        int also_max_subset = 0;
-
-        if(nums[0] == maximum) also_max_subset++;
-
-        for (int j = 1; j < nums.size(); ++j) {
-            int ors_size = ors.size();
-
-            for (int i = 0; i < ors_size; ++i) {
-                int value = ors[i] | nums[j];
-                if (value == maximum) 
-                    also_max_subset++;
-                ors.push_back(value);
+        
+        int n=nums.size();
+        int res=0,cnt=0;
+        for(int i=0;i<(1<<n);i++)
+        {
+            int ans=0;
+            for(int j=0;j<n;j++)
+            {
+                 if ((i & (1 << j)) != 0) {
+                    //cout<<nums[j]<<" ";
+                    ans|=nums[j];
+                 }
+            }
+            //cout<<"hj";
+            if(res<ans)
+            {
+                res=ans;
+                cnt=1;
+            }
+            else if(res==ans){
+                cnt++;
             }
         }
-
-        return also_max_subset;
+        return cnt;
     }
 };
