05d62ed2222f20bb15eb267532175906e0c73591
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-10-27 00:10:06 +0530

    Time: 0 ms (100%), Space: 16.5 MB (37.21%) - LeetHub

--- 2076-sum-of-digits-of-string-after-convert/2076-sum-of-digits-of-string-after-convert.py
+++ 2076-sum-of-digits-of-string-after-convert/2076-sum-of-digits-of-string-after-convert.py
@@ -1,34 +1,15 @@
 class Solution(object):
     def getLucky(self, s, k):
-
-        res = 0
-
-        C = Counter(s)
-
-        del(s)
-
-        for c in C:
-
-            a = ord(c) - 96
-            res += (a%10 + a//10)*C[c]
-
-        k -= 1
-
-        while k != 0:
-
-            if res >= 10:
-
-                p, res = res, 0
-
-                while p != 0:
-
-                    res += p%10
-                    p//=10
-
-                k -= 1
-
-                continue
-
-            break
-            
-        return res
+        # Convert each character in the string to its corresponding numeric value
+        number = ''
+        for x in s:
+            number += str(ord(x) - ord('a') + 1)
+        
+        # Perform the transformation `k` times
+        while k > 0:
+            temp = 0
+            for x in number:
+                temp += int(x)  # Sum the digits of the current number
+            number = str(temp)  # Convert the sum back to a string
+            k -= 1
+        return int(number)     
