d27a3cc9d0227aea54aa951eb7333e39eb4c5530
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-10-29 22:46:03 +0530

    Time: 0 ms (100%), Space: 67.3 MB (97.09%) - LeetHub

--- 2794-maximum-number-of-moves-in-a-grid/2794-maximum-number-of-moves-in-a-grid.cpp
+++ 2794-maximum-number-of-moves-in-a-grid/2794-maximum-number-of-moves-in-a-grid.cpp
@@ -2,22 +2,39 @@ class Solution {
 public:
     int maxMoves(vector<vector<int>>& grid) {
         int m = grid.size(), n = grid[0].size();
-    vector<pair<int, int>> dirs = {{0, 1}, {1, 1}, {-1, 1}};
-    vector<vector<int>> cache(m, vector<int>(n, -1));
+        // 無法用 sentinel： 因為左邊界也需要和左邊的格子比較大小，但只有 dp 有多一列，grid 沒有
+        // vector<int> dp(m + 2);    dp[0] = INT_MIN;    dp[m + 1] = INT_MIN;
 
-    function<int(int, int)> dp = [&](int i, int j) {
-        if (cache[i][j] != -1) return cache[i][j];
-        int ans = 0;
-        for (auto [x, y] : dirs) {
-            int ni = i + x, nj = j + y;
-            if (ni >= 0 && ni < m && nj < n && grid[i][j] < grid[ni][nj])
-                ans = max(ans, 1 + dp(ni, nj));
+        int res = 0;
+        vector<int> dp(m);
+
+        for (int j = 1; j < n; ++j) {
+            int leftTop = 0;  // 1
+            bool found = false;
+            
+            for (int i = 0; i < m; ++i) {
+                int cur = -1;
+                int nxtLeftTop = dp[i];  // next leftTop   1
+
+                if (i - 1 >= 0 && leftTop != -1 && grid[i][j] > grid[i - 1][j - 1]) {
+                    cur = max(cur, leftTop + 1);
+                }
+                if (dp[i] != -1 && grid[i][j] > grid[i][j - 1]) {
+                    cur = max(cur, dp[i] + 1);
+                }
+                if (i + 1 < m && dp[i + 1] != -1 && grid[i][j] > grid[i + 1][j - 1]) {
+                    cur = max(cur, dp[i + 1] + 1);
+                }
+                
+                dp[i] = cur;
+                found = found || (dp[i] != -1);  // check if found in this col
+                
+                leftTop = nxtLeftTop;
+            }
+            if (!found) break;
+            res = j;
         }
-        return cache[i][j] = ans;
-    };
-    int res = 0;
-    for (int i = 0; i < m; i++)
-        res = max(res, dp(i, 0));
-    return res;
+
+        return res;
     }
 };
