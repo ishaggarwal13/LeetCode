4899c8530d8a823d4cd530a7046bd83d8a5d0582
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-10-29 22:47:15 +0530

    Time: 16 ms (54.35%), Space: 74.6 MB (25.57%) - LeetHub

--- 2794-maximum-number-of-moves-in-a-grid/2794-maximum-number-of-moves-in-a-grid.cpp
+++ 2794-maximum-number-of-moves-in-a-grid/2794-maximum-number-of-moves-in-a-grid.cpp
@@ -1,23 +1,45 @@
 class Solution {
 public:
+    bool isValid(int row, int col, int rowSize, int colSize){
+        if(row<0 || row>=rowSize || col>=colSize){
+            return false;
+        }
+        return true;
+    }
+
+    int findMaxMoves(vector<vector<int>>& grid, int row, int col, int rowSize, int colSize, vector<vector<int>> &dp){
+        if(row<0 || row>=rowSize || col>=colSize){
+            return 0;
+        }
+
+        if(dp[row][col]!=-1){
+            return dp[row][col];
+        }
+        int currentCell = grid[row][col];
+        int down=0;
+        int downLeft=0;
+        int downRight=0;
+        if(isValid(row,col+1,rowSize,colSize) && grid[row][col+1] > currentCell){
+            down = 1 + findMaxMoves(grid,row,col+1,rowSize,colSize,dp);
+        }
+        if(isValid(row+1,col+1,rowSize,colSize) && grid[row+1][col+1] > currentCell){
+            downRight = 1 + findMaxMoves(grid,row+1,col+1,rowSize,colSize,dp);
+        }
+        if(isValid(row-1,col+1,rowSize,colSize) && grid[row-1][col+1] > currentCell){
+            downLeft = 1 + findMaxMoves(grid,row-1,col+1,rowSize,colSize,dp);
+        }
+
+        return dp[row][col] =  max(down,max(downLeft,downRight));
+        
+    }
+
     int maxMoves(vector<vector<int>>& grid) {
-        int m = grid.size(), n = grid[0].size();
-        int ans =0;
-        function<void(int,int)> dfs = [&] (int i, int j){
-            ans = max(ans,j);
-            if (ans == n-1){
-                return;
-            }
-            // i-1， i， i + 1
-            for (int k = max(i-1,0); k< min(i+2,m);++k) {
-                if (grid[k][j+1] > grid[i][j]) {
-                    dfs(k,j+1);
-                }
-            }
-            grid[i][j] =0;
-        };
-        for (int i=0; i< m; ++i) {
-            dfs(i,0);
+        int rowSize = grid.size();
+        int colSize = grid[0].size();
+        int ans=0;
+        vector<vector<int>> dp(rowSize,vector<int>(colSize,-1));
+        for(int i=0;i<rowSize;i++){
+            ans=max(ans,findMaxMoves(grid,i,0,rowSize,colSize,dp));
         }
         return ans;
     }
