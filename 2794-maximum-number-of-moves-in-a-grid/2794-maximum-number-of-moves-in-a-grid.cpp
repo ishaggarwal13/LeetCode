f00a20541228cc40eec18bc2dc95b168907f6a1c
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-10-29 22:46:47 +0530

    Time: 5 ms (91.3%), Space: 67.5 MB (94.82%) - LeetHub

--- 2794-maximum-number-of-moves-in-a-grid/2794-maximum-number-of-moves-in-a-grid.cpp
+++ 2794-maximum-number-of-moves-in-a-grid/2794-maximum-number-of-moves-in-a-grid.cpp
@@ -2,39 +2,23 @@ class Solution {
 public:
     int maxMoves(vector<vector<int>>& grid) {
         int m = grid.size(), n = grid[0].size();
-        // 無法用 sentinel： 因為左邊界也需要和左邊的格子比較大小，但只有 dp 有多一列，grid 沒有
-        // vector<int> dp(m + 2);    dp[0] = INT_MIN;    dp[m + 1] = INT_MIN;
-
-        int res = 0;
-        vector<int> dp(m);
-
-        for (int j = 1; j < n; ++j) {
-            int leftTop = 0;  // 1
-            bool found = false;
-            
-            for (int i = 0; i < m; ++i) {
-                int cur = -1;
-                int nxtLeftTop = dp[i];  // next leftTop   1
-
-                if (i - 1 >= 0 && leftTop != -1 && grid[i][j] > grid[i - 1][j - 1]) {
-                    cur = max(cur, leftTop + 1);
-                }
-                if (dp[i] != -1 && grid[i][j] > grid[i][j - 1]) {
-                    cur = max(cur, dp[i] + 1);
-                }
-                if (i + 1 < m && dp[i + 1] != -1 && grid[i][j] > grid[i + 1][j - 1]) {
-                    cur = max(cur, dp[i + 1] + 1);
-                }
-                
-                dp[i] = cur;
-                found = found || (dp[i] != -1);  // check if found in this col
-                
-                leftTop = nxtLeftTop;
+        int ans =0;
+        function<void(int,int)> dfs = [&] (int i, int j){
+            ans = max(ans,j);
+            if (ans == n-1){
+                return;
             }
-            if (!found) break;
-            res = j;
+            // i-1， i， i + 1
+            for (int k = max(i-1,0); k< min(i+2,m);++k) {
+                if (grid[k][j+1] > grid[i][j]) {
+                    dfs(k,j+1);
+                }
+            }
+            grid[i][j] =0;
+        };
+        for (int i=0; i< m; ++i) {
+            dfs(i,0);
         }
-
-        return res;
+        return ans;
     }
 };
