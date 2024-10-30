0740c99a6a651913c451877b70ee4f4780b49301
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-10-30 22:42:18 +0530

    Time: 11 ms (74.83%), Space: 186.7 MB (63.08%) - LeetHub

--- 2545-height-of-binary-tree-after-subtree-removal-queries/2545-height-of-binary-tree-after-subtree-removal-queries.cpp
+++ 2545-height-of-binary-tree-after-subtree-removal-queries/2545-height-of-binary-tree-after-subtree-removal-queries.cpp
@@ -11,53 +11,44 @@
  */
 class Solution {
 public:
-    unordered_map<int, pair<int, int>> mp;
-    unordered_map<int, vector<pair<int, int>>> mp2;
+    int height[100001];
+    int level[100001];
+    int levelMaxHt[100001];
+    int levelSecondMax[100001];
 
-    int height(TreeNode* root, int level = 0) 
-    {
-        if (!root)
-            return 0;
-        if (mp.find(root->val) != mp.end())
-            return mp[root->val].first;
+    int findHeight(TreeNode* root,int lvl){
+        if(root == NULL) return 0;
+        level[root->val] = lvl;
+        int left = findHeight(root->left,lvl+1);
+        int right = findHeight(root->right,lvl+1);
+        height[root->val] = 1 + max(left,right);
 
-        int l = height(root->left, level + 1);
-        int r = height(root->right, level + 1);
-        int curr = 1 + max(l, r);
-
-        mp[root->val] = {curr, level};
-
-        mp2[level].push_back({root->val, curr});
-
-        return curr;
+        if(levelMaxHt[lvl] < height[root->val]){
+            levelSecondMax[lvl] = levelMaxHt[lvl];
+            levelMaxHt[lvl] = height[root->val];
+        }
+        else if(levelSecondMax[lvl] < height[root->val]){
+            levelSecondMax[lvl] = height[root->val];
+        }
+        return height[root->val];
     }
     vector<int> treeQueries(TreeNode* root, vector<int>& queries) {
-        int H = height(root) - 1;
-        int m = queries.size();
-
-        vector<int> ans(m, H);
-        for (int i = 0; i < m; i++)
-        {
-            int q = queries[i];
-            int lev = mp[q].second;
-            int maxi = 0;
-            int secondMaxi = 0;
-            for (auto a : mp2[lev]) 
-            {
-                if (a.second > maxi) 
-                {
-                    secondMaxi = maxi;
-                    maxi = a.second;
-                } 
-                else if (a.second > secondMaxi) 
-                {
-                    secondMaxi = a.second;
-                }
-            }
-
-            if (maxi == mp[q].first) ans[i] = H - maxi + secondMaxi;
+        findHeight(root,0);
+        vector<int> result;
+        for(auto& node : queries){
+            int l = level[node];
+            int level_max_ht = levelMaxHt[l];
+            int h = height[node];
+            int ans = l +(level_max_ht == h ? levelSecondMax[l] : levelMaxHt[l]) - 1;
+            result.push_back(ans);
         }
-
-        return ans;
+        return result;
     }
 };
+/*
+1. jis node ko delete krna h vo kis level pr h or uski kitni height h aur uss level ki max height aur second max height kya h.
+2. agar jis node ko delete kr rahe ho aur ussi ki uss level pr max height h toh second max height nikalo .
+3. formula = (node jis level pr h) + (height of other nodes on that level) -1
+
+har ek node ki height aur level chahiye toh precompute it.and har levelki max height and second max height 
+*/
