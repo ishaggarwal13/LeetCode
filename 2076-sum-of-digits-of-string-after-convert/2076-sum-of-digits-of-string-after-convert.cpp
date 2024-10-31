25c185b9393ae7a7131bdf804402ad12582adefa
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-11-01 00:00:53 +0530

    Time: 2 ms (42.11%), Space: 10.5 MB (6.66%) - LeetHub

--- 2076-sum-of-digits-of-string-after-convert/2076-sum-of-digits-of-string-after-convert.cpp
+++ 2076-sum-of-digits-of-string-after-convert/2076-sum-of-digits-of-string-after-convert.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public:
+    int solve(string num, int k) {
+        if(k==0) return stoi(num);
+        int s = 0;
+        for(int i=0; i<num.length(); i++) {
+            s += (int)(num[i]-'1'+1);
+        }
+        return solve(to_string(s), k-1);
+    }
+
     int getLucky(string s, int k) {
-      int len = s.length();
-      string numstr;
-      for(char c : s)
-      {
-        numstr += to_string(c - 'a' + 1);
-      }
-      int num;
-      while(k > 0)
-      {
-        num = 0;
-      for( char c : numstr)
-      {
-        num += c - '0';
-      }
-      numstr = to_string(num);
-      k--;
-      }
-      return num;
+        string num;
+        for(int i=0; i<s.length(); i++) {
+            num += to_string((int)(s[i]-'a'+1));
+        }
+        return solve(num, k);
     }
 };
