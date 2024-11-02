54f2d550a178092a70a5c2d1747bdd73806a926a
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-11-02 19:31:31 +0530

    Time: 0 ms (100%), Space: 8.2 MB (59.78%) - LeetHub

--- 2580-circular-sentence/2580-circular-sentence.cpp
+++ 2580-circular-sentence/2580-circular-sentence.cpp
@@ -3,15 +3,11 @@ public:
     bool isCircularSentence(string sentence) {
         int n = sentence.length();
 
-        if(sentence[0] != sentence[n - 1]){
-            return false;
-        }
+        if(sentence[0] != sentence[n - 1]) return false;
         
         for(int i=0; i<n; i++){
             if(sentence[i] == ' '){
-                if(sentence[i-1] != sentence[i+1]){
-                    return false;
-                }
+                if(sentence[i-1] != sentence[i+1]) return false;
             }
         }
 
