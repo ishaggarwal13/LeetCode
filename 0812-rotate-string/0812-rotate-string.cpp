9aa0319dd5502a42fcf05faac42c6d15da196073
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-11-03 16:25:52 +0530

    Time: 0 ms (100%), Space: 7.6 MB (55.81%) - LeetHub

--- 0812-rotate-string/0812-rotate-string.cpp
+++ 0812-rotate-string/0812-rotate-string.cpp
@@ -1,25 +1,12 @@
 class Solution {
 public:
     bool rotateString(string s, string goal) {
-        int n=s.length();
-        int m=goal.length();
-
-        if(n!=m) return false;
-        int i=0;
-        while(i<n){
-
-            if(s==goal){
-                return true;
-            }
-
-            char a=s[0];
-            for( int j=0; j<n-1; j++){
-                s[j]=s[j+1];
-            }
-            s[n-1]=a;
-            i++;
-
+        if (s.length() != goal.length()) {
+            return false;
         }
-        return false;
+
+        string concatenated = goal + goal;
+
+        return concatenated.find(s) != string::npos;
     }
 };
