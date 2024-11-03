99a4e419750e6ae63109cdce0e5a82581a8ae840
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-11-03 16:26:27 +0530

    Time: 0 ms (100%), Space: 15.2 MB (16.29%) - LeetHub

--- 1766-minimum-number-of-removals-to-make-mountain-array/1766-minimum-number-of-removals-to-make-mountain-array.cpp
+++ 1766-minimum-number-of-removals-to-make-mountain-array/1766-minimum-number-of-removals-to-make-mountain-array.cpp
@@ -1,39 +1,37 @@
 class Solution {
 public:
-    vector<int> getLongestIncreasingSubsequenceLength(vector<int>& v) {
-        vector<int> lisLen(v.size(), 1);
+    vector<int> lisLength(vector<int>& v){
         vector<int> lis = {v[0]};
+        vector<int> lisLen(v.size(), 1);
 
-        for (int i = 1; i < v.size(); i++) {
+        for(int i = 1 ; i < v.size() ; i++){
             if(v[i] > lis.back()){
                 lis.push_back(v[i]);
             } else {
                 int index = lower_bound(lis.begin(), lis.end(), v[i]) - lis.begin();
                 lis[index] = v[i];
             }
-
             lisLen[i] = lis.size();
         }
-
         return lisLen;
     }
 
     int minimumMountainRemovals(vector<int>& nums) {
-        int N = nums.size();
-        vector<int> lisLength = getLongestIncreasingSubsequenceLength(nums);
+        int n = nums.size();
+        vector<int> lis = lisLength(nums);
 
         reverse(nums.begin(), nums.end());
-        vector<int> ldsLength = getLongestIncreasingSubsequenceLength(nums);
-        reverse(ldsLength.begin(), ldsLength.end());
+        vector<int> lds = lisLength(nums);
+        reverse(lds.begin(), lds.end());
 
-        int minRemovals = INT_MAX;
-        for (int i = 0; i < N; i++) {
-            if (lisLength[i] > 1 && ldsLength[i] > 1) {
-                minRemovals =
-                    min(minRemovals, N - lisLength[i] - ldsLength[i] + 1);
+        int removals = INT_MAX;
+
+        for(int i = 0 ; i < n ; i++){
+            if(lis[i] > 1 && lds[i] > 1){
+                removals = min(removals, n + 1 - lis[i] - lds[i]);
             }
         }
 
-        return minRemovals;
+        return removals;
     }
 };
