1ce3b1aa3a80c589445a77337f5887a05fbb1170
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-11-04 23:58:26 +0530

    Time: 18 ms (80.34%), Space: 29.5 MB (74.87%) - LeetHub

--- 3451-string-compression-iii/3451-string-compression-iii.cpp
+++ 3451-string-compression-iii/3451-string-compression-iii.cpp
@@ -1,28 +1,24 @@
 class Solution {
 public:
     string compressedString(string word) {
-        string temp;
-        char last=word[0];
-        int cnt=1;
-        for(int i=1; i<word.size(); i++){
-            if(last==word[i]){
-                if(cnt<9){
-                    ++cnt;
-                    continue;
-                }else{
-                    temp.push_back(cnt+'0');
-                    temp.push_back(last);
-                    cnt=1;
-                }
-            }else{
-                temp.push_back(cnt+'0');
-                temp.push_back(last);
-                cnt=1;
-                last=word[i];
+        std::ostringstream comp; // Ostringstream for efficient string building
+        int count = 1;
+        int n = word.size();
+        
+        if (n == 0) return comp.str(); // Handle empty input case early
+        
+        char ch = word[0];
+        for(int i = 1; i < n; i++) {
+            if(word[i] == ch && count < 9) { // Assuming you want a count up to 9
+                count++;
+            } else {
+                comp << count << ch; // Directly append count and character
+                ch = word[i]; // Update ch to the new character
+                count = 1; // Reset count
             }
         }
-        temp.push_back(cnt+'0');
-        temp.push_back(last);
-        return temp;
+        comp << count << ch; // Add the count and last character
+
+        return comp.str();
     }
 };
