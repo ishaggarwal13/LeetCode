ee30161fdea74d9e218537499ca111d3d82ba616
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-11-05 23:42:55 +0530

    Time: 4 ms (30.85%), Space: 12.1 MB (87.26%) - LeetHub

--- 3174-minimum-number-of-changes-to-make-binary-string-beautiful/3174-minimum-number-of-changes-to-make-binary-string-beautiful.cpp
+++ 3174-minimum-number-of-changes-to-make-binary-string-beautiful/3174-minimum-number-of-changes-to-make-binary-string-beautiful.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int minChanges(string s) {
-        int n=s.length();
-        
-        int c=0;
-        for(int i=1;i<n;i+=2){
-            if(s[i]!=s[i-1]) c++;
+        int n=s.size();
+        int ans=0;
+        for(int i=0;i<n;i+=2)
+        {
+            if(s[i]!=s[i+1])
+                ans++;
         }
-        
-        return c;
+        return ans;
     }
 };
