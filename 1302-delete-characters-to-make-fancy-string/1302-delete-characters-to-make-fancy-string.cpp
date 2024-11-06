fee0e752820cd8dffc49c89e9cc6fb1e905ca778
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-11-06 22:39:04 +0530

    Time: 33 ms (35.14%), Space: 42.7 MB (79.14%) - LeetHub

--- 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.cpp
+++ 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.cpp
@@ -2,22 +2,26 @@ class Solution {
 public:
     string makeFancyString(string s) {
         string ans = "";
-        ans.push_back(s[0]);
+        int streak = 1;
+        char last = '9';
 
-        int count = 1;
-        for(int i=1; i<s.size(); i++){
-            // if equal then inc count and check for count < 3 and add the char to ans
-            // skip the third same char
-            if(s[i] == ans.back()){
-                count++;
-                if(count < 3) ans.push_back(s[i]);
-            } else{
-                // reassign count to 1
-                // and add the char to ans
-                count = 1;
-                ans.push_back(s[i]);
+
+        for(int i = 0; i<s.size(); i++){
+            //cout << last << " " << s[i] << " " << streak << endl;
+            if(last == s[i]){
+                streak++;
+            } else {
+                last = s[i];
+                streak = 1;
             }
+            
+
+            if(streak >= 3) continue;
+
+            ans.push_back(s[i]);
+            
         }
+
         return ans;
     }
 };
