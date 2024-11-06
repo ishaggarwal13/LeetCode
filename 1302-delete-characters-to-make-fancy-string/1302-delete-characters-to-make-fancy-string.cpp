25537c9cb0f4ccc71f43dce50ead77084f803781
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-11-06 22:32:21 +0530

    Time: 15 ms (94.39%), Space: 36.4 MB (90.63%) - LeetHub

--- 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.cpp
+++ 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
     string makeFancyString(string s) {
-        string ans = "";
-        ans.push_back(s[0]);
-        int n = s.size(),cnt = 1;
-        for(int i=1;i<n;i++){
-            if(s[i] == ans.back()){
-                cnt++;
-                if(cnt < 3)ans.push_back(s[i]);
-            }
-            else{
-                cnt = 1;
-                ans.push_back(s[i]);
+        if(s.size() < 3) return s;
+
+        int j = 2; // pointer points to third element
+        for(int i=2; i<s.size(); i++){ 
+            //apply condition to check if all three are equal nothg will happen
+            // but not then at j pointer string will shift to left and move j++
+            if(s[i] != s[j-1] || s[i] != s[j-2]){
+                s[j] = s[i];
+                j++;
             }
         }
-        return ans;
+        s.resize(j);
+        return s;
     }
 };
