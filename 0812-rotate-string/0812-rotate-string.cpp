a77a43bef8176b46ad0da48c6e4fefd83e375cf5
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-11-06 22:06:14 +0530

    Time: 0 ms (100%), Space: 7.7 MB (57.23%) - LeetHub

--- 0812-rotate-string/0812-rotate-string.cpp
+++ 0812-rotate-string/0812-rotate-string.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     bool rotateString(string s, string goal) {
-        if(s.size()!= goal.size()) return false;
-        string ans= s + s;
-        if(ans.find(goal)!= string::npos){
-            return true;
-        }
-        return false;
+        if(s.size() != goal.size()) return false;
+
+        return (s+s).find(goal) != string::npos;
+        // this is working as concat s+s in which find function used to 
+        // find teh substring goal is found return the first index occourence
+        // other null which is represented by string::npos(a const indicate the invalid pos)
     }
 };
