e4726ff1dfe9567b66b188bda97207294cf007e0
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-11-06 22:10:21 +0530

    Time: 0 ms (100%), Space: 7.6 MB (57.23%) - LeetHub

--- 0812-rotate-string/0812-rotate-string.cpp
+++ 0812-rotate-string/0812-rotate-string.cpp
@@ -3,9 +3,15 @@ public:
     bool rotateString(string s, string goal) {
         if(s.size() != goal.size()) return false;
 
-        return (s+s).find(goal) != string::npos;
-        // this is working as concat s+s in which find function used to 
-        // find teh substring goal is found return the first index occourence
-        // other null which is represented by string::npos(a const indicate the invalid pos)
+        for(int i=1; i<=s.size(); i++){
+            char temp = s[0]; // storing first begin char
+
+            s.erase(s.begin()); // remove first char
+            s += temp; // add first element to last at back
+
+            if(s == goal) return true; // check for the string
+        }
+
+        return false;
     }
 };
