ccdc11d2bc2b61c91e5fabeddb5afa0b32e5f998
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-11-09 11:09:29 +0530

    Time: 0 ms (100%), Space: 9.1 MB (30.07%) - LeetHub

--- 3394-minimum-array-end/3394-minimum-array-end.cpp
+++ 3394-minimum-array-end/3394-minimum-array-end.cpp
@@ -1,31 +1,21 @@
 class Solution {
 public:
     long long minEnd(int n, int x) {
-        //Store available bit positions that are 0 in x
         vector<short> shifts;
-        
-        // Initialize result with starting number x
         long long cur = x;
-        
-        // Calculate how many 1's we need to add (n-1)
         long long calc = n - 1;
         
-        // Find all bit positions that are 0 in x (up to 32 bits)
-        for(int i = 0; i < 32; i++) 
+        for(int i = 0; i < 32; i++) //find bit positions in x which are 0
             if(!((1 << i) & x))
                 shifts.push_back(i);
-        
-        // Add remaining positions 32-63 since long long is 64 bit
         for(int i = 32; i < 64; i++)
             shifts.push_back(i);
+
+// for(auto x: shifts) cout<<x<<' ';
         
-        // Process each bit in calc (n-1)
-        // If bit is 1, set that bit in next available position from shifts
         for(int i = 0; calc > 0; i++, calc >>= 1) {
             cur += (calc & 1) << shifts[i];
         }
-        
-        // Return the final constructed number
         return cur;
     }
 };
