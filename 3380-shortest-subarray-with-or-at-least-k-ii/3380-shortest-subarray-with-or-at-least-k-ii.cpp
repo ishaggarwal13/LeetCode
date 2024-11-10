f336af91a54c57c422c5500c5ed504bc2783b1b1
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-11-10 18:37:02 +0530

    Time: 35 ms (91.21%), Space: 88.1 MB (97.77%) - LeetHub

--- 3380-shortest-subarray-with-or-at-least-k-ii/3380-shortest-subarray-with-or-at-least-k-ii.cpp
+++ 3380-shortest-subarray-with-or-at-least-k-ii/3380-shortest-subarray-with-or-at-least-k-ii.cpp
@@ -1,33 +1,65 @@
 class Solution {
 public:
-    int k;
-    void update(unsigned* freq, unsigned x, bool add=1){
-        bitset<30> b(x);
-        for(int i=0; i<30; i++)
-            freq[i]+=(add)?b[i]:-b[i];
-    }
-
-    bool check(unsigned* freq){
-        int b=0;
-        for(int i=0; i<30; i++)
-            if (freq[i]>0) b|=(1<<i);
-        return b>=k;
-    }
-
-    int minimumSubarrayLength(vector<int>& nums, int k) {
-        if (k==0) return 1;
-        this->k=k;
-        unsigned freq[30]={0};
-        int n=nums.size(), ans=INT_MAX;
-        for(int l=0, r=0; r<n; r++){
-            int x=nums[r];
-            if (x>=k) return 1;
-            update(freq, nums[r], 1);
-            while(l<n && check(freq)){
-                ans=min(ans, r-l+1);
-                update(freq, nums[l++], 0);
+    int fun(vector<int> &v){
+        int ans=0;
+        for(int i=0;i<31;i++){
+            if(v[i]>0){
+                ans+=(1<<i);
             }
         }
-        return (ans==INT_MAX)?-1:ans;
+        return ans;
+    }
+    int minimumSubarrayLength(vector<int>& nums, int k) {
+        if(k==0){
+            return 1;
+        }
+        vector<int>v(31,0);
+        int n=nums.size();
+        int j=0;
+        int ans=INT_MAX;
+        for(int i=0;i<n;i++){
+            int val=fun(v);
+            //cout<<val<<endl;
+            while(val>=k){
+                ans=min(ans,i-j);
+                //cout<<j<<" "<<i<<" "<<val<<endl;
+                int num=nums[j];
+                int cnt=0;
+                while(num>0){
+                    //cout<<num<<endl;
+                    v[cnt]-=(num &1);
+                    num=num>>1;
+                    cnt++;
+                }
+                j++;
+                val=fun(v);
+            }
+                int num=nums[i];
+                int cnt=0;
+                while(num){
+                    v[cnt]+=(num &1);
+                    num=num>>1;
+                    cnt++;
+                }
+        }
+        int val=fun(v);
+       while(val>=k){
+                ans=min(ans,n-j);
+                //cout<<j<<" "<<i<<" "<<val<<endl;
+                int num=nums[j];
+                int cnt=0;
+                while(num>0){
+                    //cout<<num<<endl;
+                    v[cnt]-=(num &1);
+                    num=num>>1;
+                    cnt++;
+                }
+                j++;
+                val=fun(v);
+            }
+        if(ans==INT_MAX){
+            return -1;
+        }
+        return ans;
     }
 };
