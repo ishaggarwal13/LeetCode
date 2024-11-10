4eb891c12c3e9285b751bd14ee76a91c82d05ad1
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-11-10 18:37:27 +0530

    Time: 30 ms (95.6%), Space: 88.2 MB (89.97%) - LeetHub

--- 3380-shortest-subarray-with-or-at-least-k-ii/3380-shortest-subarray-with-or-at-least-k-ii.cpp
+++ 3380-shortest-subarray-with-or-at-least-k-ii/3380-shortest-subarray-with-or-at-least-k-ii.cpp
@@ -1,65 +1,42 @@
 class Solution {
 public:
-    int fun(vector<int> &v){
-        int ans=0;
-        for(int i=0;i<31;i++){
-            if(v[i]>0){
-                ans+=(1<<i);
-            }
-        }
-        return ans;
-    }
     int minimumSubarrayLength(vector<int>& nums, int k) {
-        if(k==0){
-            return 1;
-        }
-        vector<int>v(31,0);
-        int n=nums.size();
-        int j=0;
-        int ans=INT_MAX;
-        for(int i=0;i<n;i++){
-            int val=fun(v);
-            //cout<<val<<endl;
-            while(val>=k){
-                ans=min(ans,i-j);
-                //cout<<j<<" "<<i<<" "<<val<<endl;
-                int num=nums[j];
-                int cnt=0;
-                while(num>0){
-                    //cout<<num<<endl;
-                    v[cnt]-=(num &1);
-                    num=num>>1;
-                    cnt++;
+        int n = nums.size();
+        int res = 1e9;
+        int l = 0, r = 0;
+        vector<int> mark(32, 0);
+        int x = 0;
+        int bit;
+        while (r < n) {
+            x |= nums[r];
+            if (nums[r] == 0)
+                bit = 0;
+            else
+                bit = 31 - __builtin_clz(nums[r]);
+            for (int i = 0; i <= bit && i < 32; i++) {
+                if ((nums[r] >> i) & 1) {
+                    mark[i]++;
                 }
-                j++;
-                val=fun(v);
             }
-                int num=nums[i];
-                int cnt=0;
-                while(num){
-                    v[cnt]+=(num &1);
-                    num=num>>1;
-                    cnt++;
+            while (l <= r && x >= k) {
+                res = min(res, r - l + 1);
+                if (nums[l] == 0)
+                    bit = 0;
+                else
+                    bit = 31 - __builtin_clz(nums[l]);
+                for (int i = 0; i <= bit && i < 32; i++) {
+                    if ((nums[l] >> i) & 1) {
+                        mark[i]--;
+                        if (mark[i] == 0) {
+                            x ^= (1 << i);
+                        }
+                    }
                 }
-        }
-        int val=fun(v);
-       while(val>=k){
-                ans=min(ans,n-j);
-                //cout<<j<<" "<<i<<" "<<val<<endl;
-                int num=nums[j];
-                int cnt=0;
-                while(num>0){
-                    //cout<<num<<endl;
-                    v[cnt]-=(num &1);
-                    num=num>>1;
-                    cnt++;
-                }
-                j++;
-                val=fun(v);
+                l++;
             }
-        if(ans==INT_MAX){
-            return -1;
+            r++;
         }
-        return ans;
+
+        return res == 1e9 ? -1 : res;
     }
 };
