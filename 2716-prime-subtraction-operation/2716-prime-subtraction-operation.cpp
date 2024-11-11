b5b594c499a619b0544eb2719ef7396a5b8e7199
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-11-11 22:44:34 +0530

    Time: 0 ms (100%), Space: 30.3 MB (14.7%) - LeetHub

--- 2716-prime-subtraction-operation/2716-prime-subtraction-operation.cpp
+++ 2716-prime-subtraction-operation/2716-prime-subtraction-operation.cpp
@@ -1,53 +1,37 @@
 class Solution {
 public:
-    bool check(int n)
-    {
-        if (n <= 1)
-		return false;
-     	if (n == 2 || n == 3)
-	     	return true;
-	    if (n % 2 == 0 || n % 3 == 0)
-		  return false;
-	    for (int i = 5; i <= sqrt(n); i = i + 6)
-		   if (n % i == 0 || n % (i + 2) == 0)
-			return false;
-	    return true;
-    }
     bool primeSubOperation(vector<int>& nums) {
-        bool fl=true;
-        int n=nums.size();
-        if(n==1) return true;
-        for(int i=1;i<n;i++)
+        vector<int> prime(1000,true);
+        for(int i=2;i *i<1000;i++)
         {
-            if(nums[i-1]>=nums[i])
+            if(prime[i])
             {
-                fl=false;
-            }
-        }
-        if(fl)
-        { 
-            // when array is already in strictly increasing order.
-            return true;
-        }
-        for(int i=nums[0]-1;i>1;i--)
-        {
-            if(check(i))
-            {
-                nums[0]-=i;
-                break;
-            }
-        }
-        for(int i=1;i<n;i++)
-        {
-            int k=nums[i]-nums[i-1];
-            if(k<=0) return false;
-            for(int j=k-1;j>1;j--)
-            {
-                if(check(j))
+                for(int j=2;j*i<1000;j++)
                 {
-                    nums[i]-=j;
-                    break;
+                    prime[j*i]=false;
                 }
+            } 
+        }
+        int n=nums.size();
+        for(int i=n-2;i>=0;i--)
+        {
+            if(nums[i]<nums[i+1])
+            continue;
+            else
+            {
+                bool flag=true;
+                for(int j=2;j<nums[i];j++)
+                {
+                    if(prime[j] && (nums[i]-j)<nums[i+1])
+                    {
+                        nums[i]=nums[i]-j;
+                        flag=false;
+                        break;
+                    }
+                }
+                if(flag)
+                return false;
+
             }
         }
         return true;
