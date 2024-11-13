8ee4c531fd2d1c9252a8128d6b6942a26b344e9a
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-11-13 22:37:29 +0530

    Time: 27 ms (94.05%), Space: 60.2 MB (80.83%) - LeetHub

--- 2699-count-the-number-of-fair-pairs/2699-count-the-number-of-fair-pairs.cpp
+++ 2699-count-the-number-of-fair-pairs/2699-count-the-number-of-fair-pairs.cpp
@@ -1,23 +1,16 @@
 class Solution {
-private:
-    int binSearch(vector<int>& nums, int l, int r, int target) {
-        while (l <= r) {
-            int m = l + (r - l) / 2;
-            if (nums[m] >= target) r = m - 1;
-            else l = m + 1;
-        }
-        return r;
-    }
 public:
-    long long countFairPairs(vector<int>& nums, int lower, int upper) {
-        sort(nums.begin(), nums.end());
-        long long res = 0;
-        for (int i = 0; i < nums.size(); i++) {
-            int low = lower - nums[i];
-            int up = upper - nums[i];
-            res += binSearch(nums, i + 1, nums.size() - 1, up + 1) - 
-                   binSearch(nums, i + 1, nums.size() - 1, low);
-        }
-        return res;
-    }
+    long long countLess(vector<int>& nums, int val) {
+    long long res = 0;
+    for (int i = 0, j = nums.size() - 1; i < j; ++i) {
+        while(i < j && nums[i] + nums[j] > val)
+            --j;
+        res += j - i;
+    }        
+    return res;
+}
+long long countFairPairs(vector<int>& nums, int lower, int upper) {
+    sort(begin(nums), end(nums));
+    return countLess(nums, upper) - countLess(nums, lower - 1);
+}
 };
