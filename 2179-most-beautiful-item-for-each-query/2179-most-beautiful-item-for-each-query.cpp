116e5a8811a176b90b35550ce4f5eccb9d171eba
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-11-13 22:38:26 +0530

    Time: 22 ms (99.22%), Space: 98.6 MB (40%) - LeetHub

--- 2179-most-beautiful-item-for-each-query/2179-most-beautiful-item-for-each-query.cpp
+++ 2179-most-beautiful-item-for-each-query/2179-most-beautiful-item-for-each-query.cpp
@@ -1,44 +1,46 @@
+struct Item {
+    int price;
+    int beauty;
+
+    bool operator<(const Item& other) const {
+        return price < other.price;
+    }
+};
+
+struct Query {
+    int threshold;
+    int index;
+
+    bool operator<(const Query& other) const {
+        return threshold < other.threshold;
+    }
+};
+
 class Solution {
 public:
     vector<int> maximumBeauty(vector<vector<int>>& items, vector<int>& queries) {
-        sort(items.begin(),items.end());
-        int n = items.size();
-        vector<int> maxBeauty(n),prices(n);
-        maxBeauty[0] = items[0][1];
-        prices[0] = items[0][0];
-        for(int i =1;i<n;i++) {
-            prices[i] = items[i][0];
-            maxBeauty[i] = max(maxBeauty[i-1],items[i][1]);
+        vector<Item> all;
+        for (const vector<int>& item : items) {
+            all.push_back({.price = item[0], .beauty = item[1]});
         }
-        int m = queries.size();
-        vector<int> ans(m);
-        for(int i = 0;i<m;i++) {
-            int idx = bs(prices,queries[i]);
-            if(idx == -1) {
-                ans[i] = 0;
-            }
-            else {
-                ans[i] = maxBeauty[idx];
-            }
-        }
-        return ans;
-    }
+        sort(all.begin(), all.end());
 
-    int bs(vector<int>& prices,int target) {
-        int low = 0;
-        int high = prices.size() - 1;
-        int result = -1;
-        while(low <= high) {
-            int mid = low + (high -low) / 2;
-            if(prices[mid] <= target) {
-                result = mid;
-                low = mid +1;
-            }
-            else {
-                high = mid - 1;
-            }
+        vector<Query> qs;
+        for (int i = 0; i < queries.size(); ++i) {
+            qs.push_back({.threshold = queries[i], .index = i});
         }
-        return result;
-    
+        sort(qs.begin(), qs.end());
+
+        vector<int> answer(queries.size());
+        int index = 0;  // index through the `all` array.
+        int max_beauty = 0;
+        for (const Query& q : qs) {
+            while (index < all.size() && all[index].price <= q.threshold) {
+                max_beauty = max(max_beauty, all[index].beauty);
+                ++index;
+            }
+            answer[q.index] = max_beauty;
+        }
+        return answer;
     }
 };
