332c2f4d7cb58f21e576f9d8a7ea79a276740eff
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-11-14 20:32:56 +0530

    Time: 0 ms (100%), Space: 74.1 MB (20.35%) - LeetHub

--- 1620-check-if-array-pairs-are-divisible-by-k/1620-check-if-array-pairs-are-divisible-by-k.cpp
+++ 1620-check-if-array-pairs-are-divisible-by-k/1620-check-if-array-pairs-are-divisible-by-k.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
     bool canArrange(vector<int>& arr, int k) {
-        int n = arr.size();
-        vector<int> remainders(k);
-        for (int i = 0; i < n; ++i) {
-            int remainder = arr[i] % k;
-            if (remainder < 0) {
-                remainder += k;
-            }
-            ++remainders[remainder];
+        vector<int> mod_count = vector<int>(k, 0);
+        for (int i = 0; i < arr.size(); i++) {
+            // Be careful about negative values.
+            mod_count[(arr[i] % k + k) % k]++;
         }
-        if (remainders[0] % 2 == 1) {
+        if (mod_count[0] % 2 == 1) {
             return false;
         }
-        for (int i = 1; i <= k / 2; ++i) {
-            if (remainders[i] != remainders[k - i]) {
-                return false;
+        for (int i = 1; i <= k / 2; i++) {
+            if (i + i == k) {
+                if (mod_count[i] % 2 == 1) {
+                    return false;
+                }
+            } else {
+                if (mod_count[i] != mod_count[k - i]) {
+                    return false;
+                }
             }
         }
         return true;
