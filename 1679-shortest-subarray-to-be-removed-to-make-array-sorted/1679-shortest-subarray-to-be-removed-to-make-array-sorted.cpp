ddde8d2affc37ea69f3536005c7001db1a0455a6
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-11-15 19:27:31 +0530

    Time: 0 ms (100%), Space: 69.6 MB (66.98%) - LeetHub

--- 1679-shortest-subarray-to-be-removed-to-make-array-sorted/1679-shortest-subarray-to-be-removed-to-make-array-sorted.cpp
+++ 1679-shortest-subarray-to-be-removed-to-make-array-sorted/1679-shortest-subarray-to-be-removed-to-make-array-sorted.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int findLengthOfShortestSubarray(vector<int>& arr) {
-        int n = arr.size();
+        const int n = arr.size();
         
         // Find longest non-decreasing prefix
         int left= 0;
@@ -9,28 +9,24 @@ public:
         
         // array is already non-decreasing
         if (left== n-1) return 0;
-
-        // Monotonic stack for  non-descreasing suffix
-        vector<int> stack={n-1};
-        for (int r= n-2; r>left; r--) {
-            if (arr[r] <= arr[r+1])
-                stack.push_back(r);
-            else break;
-        }
-
+        
+        // Find longest non-decreasing suffix
+        int right = n-1;
+        for (; right>left && arr[right-1] <= arr[right]; right--);     
+        
         // to remove as the smaller of removing all from either end
-        int remove= min(n-left-1, stack.back());
-
+        int remove = min(n-left-1, right);
+        
         // Try to merge left and right parts
-        for (int i = 0; i <=left; i++) {
-            // find j=stack.back() s.t. arr[j]>= arr[i] 
-            while (!stack.empty() && arr[i]>arr[stack.back()])
-                stack.pop_back();
-    
-            if (!stack.empty()) 
-                remove = min(remove, stack.back()-i-1);
+        for (int i = 0, j = right; i <= left && j < n; ) {
+            if (arr[i] <= arr[j]) {
+                //  remove elements  with index in (i, j)
+                remove = min(remove, j-i-1);
+                i++;
+            } 
+            else j++;  
         }
-
+        
         return remove;
     }
 };
