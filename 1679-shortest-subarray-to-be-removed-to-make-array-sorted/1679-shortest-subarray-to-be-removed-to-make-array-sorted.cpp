17629076f568540efeb5377216f96eb9221d5081
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-11-15 19:28:13 +0530

    Time: 0 ms (100%), Space: 69.6 MB (25.81%) - LeetHub

--- 1679-shortest-subarray-to-be-removed-to-make-array-sorted/1679-shortest-subarray-to-be-removed-to-make-array-sorted.cpp
+++ 1679-shortest-subarray-to-be-removed-to-make-array-sorted/1679-shortest-subarray-to-be-removed-to-make-array-sorted.cpp
@@ -1,32 +1,18 @@
 class Solution {
 public:
     int findLengthOfShortestSubarray(vector<int>& arr) {
-        const int n = arr.size();
-        
-        // Find longest non-decreasing prefix
-        int left= 0;
-        for (; left+1 < n && arr[left] <= arr[left+1]; left++); 
-        
-        // array is already non-decreasing
-        if (left== n-1) return 0;
-        
-        // Find longest non-decreasing suffix
-        int right = n-1;
-        for (; right>left && arr[right-1] <= arr[right]; right--);     
-        
-        // to remove as the smaller of removing all from either end
-        int remove = min(n-left-1, right);
-        
-        // Try to merge left and right parts
-        for (int i = 0, j = right; i <= left && j < n; ) {
-            if (arr[i] <= arr[j]) {
-                //  remove elements  with index in (i, j)
-                remove = min(remove, j-i-1);
-                i++;
-            } 
-            else j++;  
+        int n = arr.size();
+        int r = n - 1;
+        while (r > 0 && arr[r - 1] <= arr[r])
+            r--;
+        int l = 0, res = r;
+        while (l < r && (l == 0 || arr[l - 1] <= arr[l]))
+        {
+            while (r < n && arr[r] < arr[l])
+                r++;
+            res = min(res, r - l - 1);
+            l++;
         }
-        
-        return remove;
+        return res;
     }
 };
