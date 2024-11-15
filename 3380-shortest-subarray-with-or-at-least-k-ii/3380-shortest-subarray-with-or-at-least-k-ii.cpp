e650a9c34acac417106baba9abba74227a042059
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-11-15 19:33:34 +0530

    Time: 3 ms (99.01%), Space: 88 MB (93.08%) - LeetHub

--- 3380-shortest-subarray-with-or-at-least-k-ii/3380-shortest-subarray-with-or-at-least-k-ii.cpp
+++ 3380-shortest-subarray-with-or-at-least-k-ii/3380-shortest-subarray-with-or-at-least-k-ii.cpp
@@ -1,40 +1,30 @@
 class Solution {
-vector<vector<int>> bitts;
-    int answer(vector<int>& nums, int idx, int k) {
-        int left = idx, right = nums.size() - 1, ans = INT_MAX;
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            int a = 0;
-            for (int i = 0; i < 32; i++) {
-                int b = (bitts[mid + 1][i] - bitts[idx][i]);
-                if (b)
-                    a += (1 << i);
-            }
-            if (a >= k) {
-                ans = min(ans, mid - idx + 1);
-                right = mid - 1;
-            } else
-                left = mid + 1;
-        }
-        return ans;
-    }
-
 public:
     int minimumSubarrayLength(vector<int>& nums, int k) {
-        int n = nums.size();
-        int ans = INT_MAX;
-        bitts.resize(n + 1, vector<int>(32, 0));
-        for (int i = 0; i < n; i++)
-            for (int j = 0; j < 32; j++)
-                bitts[i + 1][j] = bitts[i][j] + ((nums[i] >> j) & 1);
-
-        for (int i = 0; i < n; i++) {
-            int temp = answer(nums, i, k);
-            if (temp != INT_MAX)
-                ans = min(ans, temp);
-            else
-                break;
+        if (k==0) return 1;
+        int n = nums.size(), ans = 0, i = 0, j = 0, curOr = 0, curAnd = 1, curXor = 0;
+        ans = n+1;
+        while (i<n) {
+            //cout<<"~~~"<<i<<",,,"<<j<<endl;
+            //cout<<curOr<<", "<<curAnd<<", "<<curXor<<endl;
+            if (curOr<k && j<n) {
+                curOr |= nums[j++];
+            } else if (curOr>=k) {
+                ans = min(ans, j-i);
+                i++;
+                if (i<n && nums[i]==nums[i-1]) {
+                    if (curOr==nums[i])
+                        curOr = 0;
+                    continue;
+                }
+                curOr = 0;
+                for (int k=i; k<j; ++k) {
+                    curOr |= nums[k];
+                }
+            } else {
+                i++;
+            }
         }
-        return ans == INT_MAX ? -1 : ans;
+        return ans>n? -1: ans;
     }
 };
