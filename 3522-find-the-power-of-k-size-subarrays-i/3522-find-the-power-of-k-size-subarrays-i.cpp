5a0256e6d00a9f42138390efc662102a5aadc8f4
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-11-16 21:45:30 +0530

    Time: 4 ms (33.33%), Space: 33.2 MB (32.15%) - LeetHub

--- 3522-find-the-power-of-k-size-subarrays-i/3522-find-the-power-of-k-size-subarrays-i.cpp
+++ 3522-find-the-power-of-k-size-subarrays-i/3522-find-the-power-of-k-size-subarrays-i.cpp
@@ -1,25 +1,27 @@
 class Solution {
 public:
     vector<int> resultsArray(vector<int>& nums, int k) {
-        int len = nums.size();
-        vector<int> res(len-k+1,-1);
-        deque<int> indexDeque;
-        for(int i=0;i<len;i++)
-        {
-            if(!indexDeque.empty() && indexDeque.front() < i-k+1)
-            indexDeque.pop_front();
-
-            if(!indexDeque.empty() && nums[i] != nums[i-1] +1)
-            indexDeque.clear();
-
-            indexDeque.push_back(i);
-
-            if(i >= k-1)
-            {
-                if(indexDeque.size()==k)
-                res[i-k+1] = nums[indexDeque.back()];
+        vector<int> res;
+        int l = 0;
+        int consec_cnt = 1;
+        
+        for (int r = 0; r < nums.size(); r++) {
+            if (r > 0 && nums[r - 1] + 1 == nums[r]) {
+                consec_cnt++;
+            }
+            
+            if (r - l + 1 > k) {
+                if (nums[l] + 1 == nums[l + 1]) {
+                    consec_cnt--;
+                }
+                l++;
+            }
+            
+            if (r - l + 1 == k) {
+                res.push_back(consec_cnt == k ? nums[r] : -1);
             }
         }
+        
         return res;
     }
 };
