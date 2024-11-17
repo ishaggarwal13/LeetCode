73e0d5175a9eadd9c847c465ebc0383f866abdfc
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-11-17 23:33:04 +0530

    Time: 21 ms (96.03%), Space: 111.1 MB (60.47%) - LeetHub

--- 0892-shortest-subarray-with-sum-at-least-k/0892-shortest-subarray-with-sum-at-least-k.cpp
+++ 0892-shortest-subarray-with-sum-at-least-k/0892-shortest-subarray-with-sum-at-least-k.cpp
@@ -1,35 +1,25 @@
-const int MAX_N = 100000 + 1;
-
-int* nums;
-
-long long st_s[MAX_N];
-int pos[MAX_N];
-
 class Solution {
 public:
-    int shortestSubarray(vector<int>& _nums, int k) {
-        int n = _nums.size();
-        nums = &_nums[0];
-        
+    int shortestSubarray(vector<int>& nums, int k) {
+        int n = nums.size();
+        std::vector<std::pair<long long, int>> st(n + 1);
         long long sp = 0LL;
         int res = n + 1;
 
         int b = 0, e = 0;
-        st_s[e] = 0;
-        pos[e++] = -1;
+        st[e++] = {0, -1};
 
         for (int i = 0; i < n; i++) {
             sp += nums[i];
 
-            while (b < e && sp - st_s[b] >= k) {
-                res = std::min(res, i - pos[b]);
+            while (b < e && sp - st[b].first >= k) {
+                res = std::min(res, i - st[b].second);
                 b++;
             }
 
-            while (b < e && sp <= st_s[e - 1])
+            while (b < e && sp <= st[e - 1].first)
                 e--;
-            st_s[e] = sp;
-            pos[e++] = i;
+            st[e++] = {sp, i};
         }
 
         if (res == n + 1)
