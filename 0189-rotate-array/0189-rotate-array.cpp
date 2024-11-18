02871faf3ace3bbad0e31c10aadd81f9c2079055
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-11-18 21:25:38 +0530

    Time: 0 ms (100%), Space: 27.6 MB (80.94%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,11 +1,20 @@
 class Solution {
 public:
+    void reverse(vector<int>& nums, int low, int high){
+        while(low < high){
+            int temp = nums[low];
+            nums[low] = nums[high];
+            nums[high] = temp;
+            low++;
+            high--;
+        }
+    }
     void rotate(vector<int>& nums, int k) {
         int n = nums.size();
         k = k % n;
 
-        reverse(nums.begin(), nums.end());
-        reverse(nums.begin(), nums.begin() + k);
-        reverse(nums.begin() + k, nums.end());
+        reverse(nums, 0, n-1);
+        reverse(nums, 0, k-1);
+        reverse(nums, k, n-1);
     }
 };
