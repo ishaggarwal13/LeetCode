bc512e63fc66730d172ce589c4cd3fced4c6d18d
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-11-18 21:49:08 +0530

    Time: 0 ms (100%), Space: 10.4 MB (60.07%) - LeetHub

---
+++ 1755-defuse-the-bomb/1755-defuse-the-bomb.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> decrypt(vector<int>& code, int k) {
+        //brute force tc - O(n*k) and sc - O(n)
+        int n = code.size();
+        vector<int> ans(n, 0);
+
+        for(int i=0; i<code.size(); i++){
+            if(k>0) {
+                for(int j=i+1; j<i+1+k; j++){
+                    ans[i] += code[j%n];
+                }
+            }
+            else if(k<0){
+                for(int j=i-1; j>i-1-abs(k); j--){
+                    ans[i] += code[((j%n)+n) % n];
+                }
+            }
+        }
+        return ans;
+    }
+};
