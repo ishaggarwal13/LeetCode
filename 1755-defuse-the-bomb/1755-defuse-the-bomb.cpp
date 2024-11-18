e748f59d5092c3a5c9935b5c2bb23bf0c94b1a6e
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-11-18 21:58:42 +0530

    Time: 0 ms (100%), Space: 10.2 MB (84%) - LeetHub

--- 1755-defuse-the-bomb/1755-defuse-the-bomb.cpp
+++ 1755-defuse-the-bomb/1755-defuse-the-bomb.cpp
@@ -1,22 +1,27 @@
 class Solution {
 public:
-    vector<int> decrypt(vector<int>& circ, int k) {
-        int n = circ.size(); // circular array
-        vector<int> result(n);
+    vector<int> decrypt(vector<int>& code, int k) {
+        int n = code.size(); // circular array
+        vector<int> result(n, 0);
 
         if (k == 0) return result;
 
-        int wSum = 0; 
-        int start = (k > 0) ? 1 : n + k;
-        int end = (k > 0) ? k : n - 1;
-
-        for (int i = start; i <= end; i++) {
-            wSum += circ[i % n];
+        if(k>0){
+            int sum = accumulate(code.begin()+1, code.begin()+k+1, 0);
+            result[0] = sum;
+            for(int left=1, right=k+1; left<n; right++, left++){
+                sum += -code[left] + code[right%n];
+                result[left] = sum;
+            }
+            return result;
         }
-        for (int i = 0; i < n; i++) {
-            result[i] = wSum;
-            wSum -= circ[(start + i) % n];
-            wSum += circ[(end + i + 1) % n];
+
+        k = -k;
+        int sum = accumulate(code.end()-k, code.end(), 0);
+        result[0] = sum;
+        for(int right=0, left=n-k; right<n-1; right++, left++){
+            sum += -code[left%n] + code[right];
+            result[right+1] = sum;
         }
         
         return result;
