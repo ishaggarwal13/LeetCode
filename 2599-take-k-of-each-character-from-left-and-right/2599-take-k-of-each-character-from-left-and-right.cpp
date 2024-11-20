e71894f59beb619d6f4546550b8f7906a4717f3b
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-11-20 22:23:51 +0530

    Time: 15 ms (59.02%), Space: 12.2 MB (56.21%) - LeetHub

--- 2599-take-k-of-each-character-from-left-and-right/2599-take-k-of-each-character-from-left-and-right.cpp
+++ 2599-take-k-of-each-character-from-left-and-right/2599-take-k-of-each-character-from-left-and-right.cpp
@@ -1,28 +1,34 @@
 class Solution {
 public:
     int takeCharacters(string s, int k) {
-        int ca=0,cb=0,cc=0;
-        int n=s.size();
-        int ans=n;
-        for(int i=0;i<n;i++){
-            if(s[i]=='a') ca++;
-            if(s[i]=='b') cb++;
-            if(s[i]=='c') cc++;
+        int n = s.size();
+        if (k == 0) return 0;
+        int countA = 0, countB = 0, countC = 0;
+        for (char &ch : s) {
+            if (ch == 'a') countA++;
+            else if (ch == 'b') countB++;
+            else countC++;
         }
-        if(ca<k||cb<k||cc<k) return -1;
-        int i=n-1,j=n-1;
-        while(i>=0){
-            if(s[i]=='a') ca--;
-            if(s[i]=='b') cb--;
-            if(s[i]=='c') cc--;
-            while(ca<k||cb<k||cc<k){
-            if(s[j]=='a') ca++;
-            if(s[j]=='b') cb++;
-            if(s[j]=='c') cc++;
-                j--;
+        int a = countA - k;
+        int b = countB - k;
+        int c = countC - k;
+        if (a < 0 || b < 0 || c < 0) return -1;
+        int left = 0, right = 0;
+        int maxLen = 0;
+        int c_a = 0, c_b = 0, c_c = 0;
+        while (right < n) {
+            if (s[right] == 'a') c_a++;
+            else if (s[right] == 'b') c_b++;
+            else c_c++;
+            while (left <= right && (c_a > a || c_b > b || c_c > c)) {
+                if (s[left] == 'a') c_a--;
+                else if (s[left] == 'b') c_b--;
+                else c_c--;
+                left++;
             }
-            ans=min(ans,i+n-1-j); i--;
+            maxLen = max(maxLen, right - left + 1);
+            right++;
         }
-        return ans;
+        return n - maxLen;
     }
 };
