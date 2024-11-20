fcc1439eb37f3e5978fabafc0a4530961e566305
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-11-20 22:24:19 +0530

    Time: 11 ms (69.76%), Space: 12.1 MB (56.21%) - LeetHub

--- 2599-take-k-of-each-character-from-left-and-right/2599-take-k-of-each-character-from-left-and-right.cpp
+++ 2599-take-k-of-each-character-from-left-and-right/2599-take-k-of-each-character-from-left-and-right.cpp
@@ -1,34 +1,34 @@
 class Solution {
 public:
     int takeCharacters(string s, int k) {
-        int n = s.size();
-        if (k == 0) return 0;
-        int countA = 0, countB = 0, countC = 0;
-        for (char &ch : s) {
-            if (ch == 'a') countA++;
-            else if (ch == 'b') countB++;
-            else countC++;
+        vector<int>v(3,0);
+        int n=s.length();
+        int a=0,b=0,c=0;
+        if(k==0)return 0;
+        for(int i=0;i<n;i++){
+           v[s[i]-'a']++;
         }
-        int a = countA - k;
-        int b = countB - k;
-        int c = countC - k;
-        if (a < 0 || b < 0 || c < 0) return -1;
-        int left = 0, right = 0;
-        int maxLen = 0;
-        int c_a = 0, c_b = 0, c_c = 0;
-        while (right < n) {
-            if (s[right] == 'a') c_a++;
-            else if (s[right] == 'b') c_b++;
-            else c_c++;
-            while (left <= right && (c_a > a || c_b > b || c_c > c)) {
-                if (s[left] == 'a') c_a--;
-                else if (s[left] == 'b') c_b--;
-                else c_c--;
-                left++;
+        for(int i=0;i<3;i++){
+            if(v[i]<k)return -1;
+        }
+         
+         int ans=0;
+         int i=0,j=0;
+        vector<int>res(3,0);
+
+        for(i=0;i<n;i++){
+            res[s[i]-'a']++;
+
+            while(j<=i && (v[0]-res[0]<k || v[1]-res[1]<k || v[2]-res[2]<k)){
+                res[s[j]-'a']--;
+                j++;
+                  
             }
-            maxLen = max(maxLen, right - left + 1);
-            right++;
+          ans=max(ans,i-j+1);
+             
+
         }
-        return n - maxLen;
+       
+        return n-ans;
     }
 };
