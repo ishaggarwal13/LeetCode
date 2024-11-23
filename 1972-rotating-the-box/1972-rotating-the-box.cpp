7624199e9d1b36b81f17aec643b4954c7571eeb7
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-11-23 15:56:15 +0530

    Time: 205 ms (26.25%), Space: 56.4 MB (25.37%) - LeetHub

--- 1972-rotating-the-box/1972-rotating-the-box.cpp
+++ 1972-rotating-the-box/1972-rotating-the-box.cpp
@@ -1,25 +1,30 @@
 class Solution {
 public:
     vector<vector<char>> rotateTheBox(vector<vector<char>>& box) {
-        int ROWS = box.size();
-        int COLS = box[0].size();
+        int m = box.size();
+        int n = box[0].size();
         
-        vector<vector<char>> res(COLS, vector<char>(ROWS, '.'));
-        
-        for (int r = 0; r < ROWS; r++) {
-            int i = COLS - 1;
-            for (int c = COLS - 1; c >= 0; c--) {
-                if (box[r][c] == '#') {
-                    res[i][ROWS - r - 1] = '#';
-                    i--;
+        for (auto& row : box) {
+            int dropPos = n - 1;
+            
+            for (int currPos = n - 1; currPos >= 0; currPos--) {
+                if (row[currPos] == '*') {
+                    dropPos = currPos - 1;
                 }
-                else if (box[r][c] == '*') {
-                    res[c][ROWS - r - 1] = '*';
-                    i = c - 1;
+                else if (row[currPos] == '#') {
+                    swap(row[dropPos], row[currPos]);
+                    dropPos--;
                 }
             }
         }
         
-        return res;
+        vector<vector<char>> rotatedBox(n, vector<char>(m));
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                rotatedBox[j][m - 1 - i] = box[i][j];
+            }
+        }
+        
+        return rotatedBox;
     }
 };
