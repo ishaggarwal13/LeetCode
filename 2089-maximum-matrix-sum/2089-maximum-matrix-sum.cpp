d36c38da5b1ef485f17aea44376d5f393b350296
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-11-25 04:31:24 +0530

    Time: 0 ms (100%), Space: 38.4 MB (31.4%) - LeetHub

--- 2089-maximum-matrix-sum/2089-maximum-matrix-sum.cpp
+++ 2089-maximum-matrix-sum/2089-maximum-matrix-sum.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
     long long maxMatrixSum(vector<vector<int>>& matrix) {
-        int minValue = INT_MAX;
-        long long sum = 0;
-        int negCount = 0;
+        int ct=0;
+        long long sum=0,minm=1e9;
 
-        for (int i = 0; i < matrix.size(); i++) {
-            for (int j = 0; j < matrix[0].size(); j++) {
-                if (matrix[i][j] < 0)
-                    negCount++;
-                int absValue = abs(matrix[i][j]);
-                minValue = min(minValue, absValue);
-                sum += absValue;
+        for(int i=0;i<matrix.size();i++)
+        {
+            for(int j=0;j<matrix[i].size();j++)
+            {
+                if(matrix[i][j]<0)
+                ct++;
+
+                sum+=abs(matrix[i][j]);
+
+                minm=min(minm ,(long long) abs(matrix[i][j]));
             }
         }
 
-        if (negCount % 2 == 0)
-            return sum;
-        return sum - 2 * minValue;
+        if(ct%2==0)
+        return sum;
+
+        return sum-2*minm;
     }
 };
