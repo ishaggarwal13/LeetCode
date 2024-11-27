275811207e2717199b8f09eb064b2b02c5b983fe
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-11-27 22:12:45 +0530

    Time: 11 ms (98.81%), Space: 52 MB (98.54%) - LeetHub

--- 3517-shortest-distance-after-road-addition-queries-i/3517-shortest-distance-after-road-addition-queries-i.cpp
+++ 3517-shortest-distance-after-road-addition-queries-i/3517-shortest-distance-after-road-addition-queries-i.cpp
@@ -1,42 +1,30 @@
 class Solution {
+    void dfs(vector<vector<int>>& tree, int c, vector<int>& dp) {
+        int d=dp[c]+1;
+        for (int x : tree[c]) {
+            if (dp[x]<=d) continue;
+            dp[x]=d;
+            dfs(tree,x,dp);
+        }
+    }
 public:
     vector<int> shortestDistanceAfterQueries(int n, vector<vector<int>>& queries) {
-        vector<vector<int>> adj(n);
-        for(int i = 0; i < n - 1; i++)
-        {
-            adj[i].push_back(i+1);
+        vector<int> dp(n);
+        for (int i=0; i<n; ++i) dp[i]=n-1-i;
+        vector<vector<int>> tree(n);
+        for (int i=0; i+1<n; ++i)
+            tree[i+1].push_back(i);
+        int m=int(queries.size());
+        vector<int> res(m);
+        int i=0;
+        for (auto& q : queries) {
+            int a=q[0], b=q[1];
+            tree[b].push_back(a);
+            dp[a]=min(dp[a],dp[b]+1);
+            dfs(tree,a,dp);
+            res[i]=dp[0];
+            ++i;
         }
-
-        vector<int> dist,result;
-        
-        for(int i = 0; i < n; i++)
-        {
-            dist.push_back(i);
-        }
-        
-        for(const auto &qi: queries) {
-            int src = qi[0];
-            int dest = qi[1];
-            adj[src].push_back(dest);
-            
-            if (dist[src] + 1 < dist[dest]) {
-                queue<int> q;
-                q.push(dest);
-                dist[dest] = dist[src] + 1;
-                
-                while (!q.empty()) {
-                    int v = q.front();
-                    q.pop();
-                    for (auto &next : adj[v]) {
-                        if (dist[v] + 1 < dist[next]) {
-                            dist[next] = dist[v] + 1;
-                            q.push(next);
-                        }
-                    }
-                }
-            } 
-            result.push_back(dist.back());
-        }
-        return result;
+        return res;
     }
 };
