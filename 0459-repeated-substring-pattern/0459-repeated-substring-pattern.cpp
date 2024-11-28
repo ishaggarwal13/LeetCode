00e6d1316d102e91b5cde6b7eadc141262e31f35
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-11-28 22:58:57 +0530

    Time: 10 ms (57.6%), Space: 13.9 MB (85.9%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
@@ -1,10 +1,17 @@
 class Solution {
 public:
     bool repeatedSubstringPattern(string s) {
-         string doubled = s + s; 
-        string modified = doubled.substr(1, doubled.size() - 2);
+        for(int i = 1; i <= s.length()/2; i++){
+            if(not ((s.length()%(i))== 0)) continue;
 
-        return modified.find(s) != string::npos;
+            string sub = s.substr(0, i);
+            for(int j = 0; j < s.length(); j++){
+                if(s[j] != sub[j%i]) break;
+                if(j==s.length()-1) return true;                
+            }
+
+        }  
+        return false;
         
     }
 };
