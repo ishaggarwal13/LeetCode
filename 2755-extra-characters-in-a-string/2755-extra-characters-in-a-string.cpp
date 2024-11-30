06b023b5324ae188e0ac86754939b9372f8e6cb2
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-11-30 21:46:01 +0530

    Time: 40 ms (91.93%), Space: 60.8 MB (97.87%) - LeetHub

--- 2755-extra-characters-in-a-string/2755-extra-characters-in-a-string.cpp
+++ 2755-extra-characters-in-a-string/2755-extra-characters-in-a-string.cpp
@@ -1,29 +1,27 @@
 class Solution {
 public:
     int minExtraChar(string s, vector<string>& dictionary) {
-        int n = s.size();
-        unordered_map<string, int> mp;
-        for(string& word : dictionary) mp[word]++;
-        vector<int> dp(n+1, 0);
+         memset(dp, -1, sizeof(dp)); // Initialize dp array with -1
+        
+        return minExtraCharHelper(s, dictionary, 0);
+    }
+    private:
+    int dp[51]; // Initialize dp array to store memoization values
 
-        for(int index = n-1; index>=0; index--){
-            string currStr = "";
-            int minExtra = s.size();
+    int minExtraCharHelper(const string &s, vector<string>& dict, int i) {
+        if (i == s.size())
+            return 0;
 
-            for (int cutIdx = index; cutIdx < n; cutIdx++)
-            {
-                currStr.push_back(s[cutIdx]);
-                //currStr will be a string from (index to cutIdx)
+        if (dp[i] == -1) {
+            dp[i] = 1 + minExtraCharHelper(s, dict, i + 1); // Initialize with one extra character.
 
-                //if string not in dictionary, we have to delete as they are extra chars
-                int currExtra = (mp.count(currStr))? 0 : currStr.size();
-                int nextExtra = dp[cutIdx + 1]; 
-                int totalExtra = currExtra + nextExtra;
-
-                minExtra = min(minExtra, totalExtra);
+            for (const auto &w : dict) {
+                if (s.compare(i, w.size(), w) == 0) {
+                    dp[i] = min(dp[i], minExtraCharHelper(s, dict, i + w.size())); // Update if a word in the dictionary is found.
+                }
             }
-            dp[index] = minExtra;
         }
-        return dp[0];
+
+        return dp[i];
     }
 };
