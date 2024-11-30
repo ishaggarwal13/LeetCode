a54af4395aff7b90c261003d520d6e6bcbc6af4f
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-11-30 20:33:06 +0530

    Time: 519 ms (68.57%), Space: 344.9 MB (34.91%) - LeetHub

--- 2201-valid-arrangement-of-pairs/2201-valid-arrangement-of-pairs.cpp
+++ 2201-valid-arrangement-of-pairs/2201-valid-arrangement-of-pairs.cpp
@@ -1,52 +1,55 @@
 class Solution {
 public:
     vector<vector<int>> validArrangement(vector<vector<int>>& pairs) {
-        unordered_map<int, vector<int>> graph;
-        unordered_map<int, int> inDegree, outDegree;
-
-        // Build the graph and calculate in/out-degrees
-        for (auto& pair : pairs) {
-            int u = pair[0], v = pair[1];
-            graph[u].push_back(v);
-            outDegree[u]++;
-            inDegree[v]++;
+        unordered_set<int> node_set;
+        unordered_map<int, int> indegree_map, outdegree_map;
+        unordered_map<int, unordered_set<int>> neighbour_map;
+        for (int i = 0; i < pairs.size(); i++) {
+            int u = pairs[i][0], v = pairs[i][1];
+            node_set.insert(u);
+            node_set.insert(v);
+            outdegree_map[u]++;
+            indegree_map[v]++;
+            neighbour_map[u].insert(v);
         }
 
-        // Find the starting node (vertex with out-degree > in-degree)
-        int startNode = pairs[0][0];
-        for (auto& [node, _] : graph) {
-            if (outDegree[node] > inDegree[node]) {
-                startNode = node;
+        // Determine the starting point.
+        int current = *node_set.begin();
+        for (int node : node_set) {
+            if (indegree_map.find(node) == indegree_map.end()) {
+                indegree_map[node] = 0;
+            }
+            if (outdegree_map.find(node) == outdegree_map.end()) {
+                outdegree_map[node] = 0;
+            }
+            if (outdegree_map[node] - indegree_map[node] == 1) {
+                current = node;
                 break;
             }
         }
 
-        // Hierholzer's algorithm for finding the Eulerian path
-        stack<int> stk;
-        deque<int> eulerPath;
-        stk.push(startNode);
-
-        while (!stk.empty()) {
-            int node = stk.top();
-
-            if (!graph[node].empty()) {
-                int nextNode = graph[node].back();
-                graph[node].pop_back();
-                stk.push(nextNode);
+        // Find the Eulerian path.
+        stack<int> node_stack;
+        vector<vector<int>> result;
+        while (true) {
+            if (!neighbour_map[current].empty()) {
+                node_stack.push(current);
+                int next = *(neighbour_map[current].begin());
+                neighbour_map[current].erase(next);
+                current = next;
             } else {
-                eulerPath.push_front(node);
-                stk.pop();
+                if (!node_stack.empty()) {
+                    int prev = node_stack.top();
+                    node_stack.pop();
+                    result.push_back({prev, current});
+                    current = prev;
+                } else {
+                    break;
+                }
             }
         }
 
-        // Convert Eulerian path into the required arrangement
-        vector<vector<int>> result;
-        auto it = eulerPath.begin();
-        while (it + 1 != eulerPath.end()) {
-            result.push_back({*it, *(it + 1)});
-            it++;
-        }
-
+        reverse(result.begin(), result.end());
         return result;
     }
 };
