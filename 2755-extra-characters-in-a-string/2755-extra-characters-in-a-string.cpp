c5f8937aadfc043d51408effcbc058b46f8e1526
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-11-30 21:45:33 +0530

    Time: 47 ms (89.01%), Space: 71.5 MB (76.54%) - LeetHub

--- 2755-extra-characters-in-a-string/2755-extra-characters-in-a-string.cpp
+++ 2755-extra-characters-in-a-string/2755-extra-characters-in-a-string.cpp
@@ -1,26 +1,29 @@
 class Solution {
 public:
-    int dp[100];
-    int solve(string &s, vector<string> &dict, int ptr){
-        
-        if(ptr >= s.size()) return 0;
-        if(dp[ptr]!=-1) return dp[ptr];
-        // now you can either a single character equal to s[ptr] and get ans from rest 
-        int easyWay = 1 + solve(s,dict,ptr+1);
-        int tougherWay = INT_MAX; // it involves use one of the words from dict
-        for(auto &x : dict){
-            // check if remaining chars are compatible first
-            bool right = true;
-            for(int i = 0; i<x.size();++i){
-                if(ptr + x.size()  > s.size() || x[i] != s[ptr+i]) {right = false; break;}
+    int minExtraChar(string s, vector<string>& dictionary) {
+        int n = s.size();
+        unordered_map<string, int> mp;
+        for(string& word : dictionary) mp[word]++;
+        vector<int> dp(n+1, 0);
+
+        for(int index = n-1; index>=0; index--){
+            string currStr = "";
+            int minExtra = s.size();
+
+            for (int cutIdx = index; cutIdx < n; cutIdx++)
+            {
+                currStr.push_back(s[cutIdx]);
+                //currStr will be a string from (index to cutIdx)
+
+                //if string not in dictionary, we have to delete as they are extra chars
+                int currExtra = (mp.count(currStr))? 0 : currStr.size();
+                int nextExtra = dp[cutIdx + 1]; 
+                int totalExtra = currExtra + nextExtra;
+
+                minExtra = min(minExtra, totalExtra);
             }
-            if(!right) continue;
-            tougherWay = min(tougherWay,solve(s,dict,ptr+x.size()));
+            dp[index] = minExtra;
         }
-        return dp[ptr] = min(easyWay,tougherWay);
-    }
-    int minExtraChar(string s, vector<string>& dict) {
-        for(int i = 80; i>=0;--i) dp[i] = -1;
-        return solve(s,dict,0);
+        return dp[0];
     }
 };
