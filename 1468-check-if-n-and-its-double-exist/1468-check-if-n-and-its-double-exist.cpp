4b0e37d449672d193e89263a2296942db33e8bf6
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-12-01 10:42:00 +0530

    Time: 5 ms (9.14%), Space: 14 MB (15.21%) - LeetHub

--- 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.cpp
+++ 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.cpp
@@ -1,16 +1,25 @@
 class Solution {
 public:
     bool checkIfExist(vector<int>& arr) {
-        // Step 1: Iterate through all pairs of indices
-        for (int i = 0; i < arr.size(); i++) {
-            for (int j = 0; j < arr.size(); j++) {
-                // Step 2: Check the conditions
-                if (i != j && arr[i] == 2 * arr[j]) {
+        unordered_set<int>us;
+        
+        for(int i = 0; i<arr.size(); i++){
+		//if the double of the element existed previously in the array
+            if(us.find(arr[i]*2) != us.end()){
+                return true;
+            }
+			
+       //as the array is of integers so no need to check for the half of odd numbers
+	   //here we have checked if the number is even if yess ...then check if there is half of the current number present previously in the array
+            if(arr[i]%2 == 0){
+                if(us.find(arr[i]/2) != us.end()){
                     return true;
                 }
             }
+          //if none of the above cases are true then we will insert the current element into the unordered set and will move further checking other elements
+            us.insert(arr[i]);
         }
-        // No valid pair found
+        
         return false;
     }
 };
