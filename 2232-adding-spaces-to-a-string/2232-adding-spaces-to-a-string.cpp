51bd38956cacac87293e19500031e20f56f2a03a
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-12-03 11:22:52 +0530

    Time: 12 ms (92.77%), Space: 82.4 MB (88.45%) - LeetHub

--- 2232-adding-spaces-to-a-string/2232-adding-spaces-to-a-string.cpp
+++ 2232-adding-spaces-to-a-string/2232-adding-spaces-to-a-string.cpp
@@ -1,23 +1,15 @@
 class Solution {
 public:
     string addSpaces(string s, vector<int>& spaces) {
-        // Final length of the result
-        int totalLength = s.size() + spaces.size(); 
-        // Pre-allocated buffer
-        char result[totalLength]; 
-        int sIndex = 0, spacesIndex = 0, resultIndex = 0;
-
-        while (sIndex < s.size()) {
-            if (spacesIndex < spaces.size() && sIndex == spaces[spacesIndex]) {
-                // Insert space at the correct position
-                result[resultIndex++] = ' '; 
-                spacesIndex++;
+        std::string result(s.size() + spaces.size(), ' ');
+        size_t tracker = 0, k = 0;
+        for (size_t i = 0; i < s.size(); ++i, ++k) {
+            if (tracker < spaces.size() and i == spaces[tracker]) {
+                ++tracker;
+                ++k;
             }
-            // Copy the current character
-            result[resultIndex++] = s[sIndex++]; 
+            result[k] = s[i];
         }
-
-        // Convert the buffer to a string
-        return string(result, totalLength); 
+        return result;
     }
 };
