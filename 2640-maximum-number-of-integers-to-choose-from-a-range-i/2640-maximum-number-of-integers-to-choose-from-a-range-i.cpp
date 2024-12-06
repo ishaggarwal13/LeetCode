e82edb1a562472205ddeb8b2a806dc4567bad313
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-12-06 19:30:00 +0530

    Time: 60 ms (84.35%), Space: 117.5 MB (88.15%) - LeetHub

--- 2640-maximum-number-of-integers-to-choose-from-a-range-i/2640-maximum-number-of-integers-to-choose-from-a-range-i.cpp
+++ 2640-maximum-number-of-integers-to-choose-from-a-range-i/2640-maximum-number-of-integers-to-choose-from-a-range-i.cpp
@@ -1,14 +1,43 @@
 class Solution {
 public:
     int maxCount(vector<int>& banned, int n, int maxSum) {
+        int ret = 0;
+        int prev = 0;
+        int sum = 0;
+        int il = banned.size();
         sort(banned.begin(), banned.end());
-    int ans = 0;
-    for(int i = 1, j = 0; i <= n; ++i){
-        if((j == banned.size() || i < banned[j] ) && maxSum- i >= 0) { 
-            ans++; maxSum -= i; 
+        for (int i = 0; i < il; i++) {
+            if (sum < maxSum && n > prev) {
+                int mn = min(banned[i] - 1, n);
+                int cnt = mn-prev;
+                if (cnt > 0) {
+                    //cout << arr[i] << ", cnt: " << cnt << endl;
+                    int sum2 = ((prev + 1) * cnt) + (cnt * (cnt - 1)) / 2;
+                    while(cnt > 0 && (sum + sum2) > maxSum) {
+                        sum2 -= mn;
+                        mn--;
+                        cnt--;
+                    }
+                    if (cnt == 0) break;
+                    sum += sum2;
+                    ret += cnt;
+                }
+                prev = banned[i];
+            } else {
+                break;
+            }
         }
-        while(j < banned.size() && i >= banned[j]) j++;
-    }
-    return ans;
+        if (sum < maxSum && n > prev) {
+            int mn = n;
+            int cnt = n-prev;
+            int sum2 = ((prev + 1) * cnt) + (cnt * (cnt - 1)) / 2;
+            while(cnt > 0 && (sum + sum2) > maxSum) {
+                sum2 -= mn;
+                mn--;
+                cnt--;
+            }
+            ret += cnt;
+        }
+        return ret;
     }
 };
