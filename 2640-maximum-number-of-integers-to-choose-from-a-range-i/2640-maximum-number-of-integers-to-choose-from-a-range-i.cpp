ccf62009260461219437f90f6a9ae87f43d6cd32
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-12-06 19:29:15 +0530

    Time: 69 ms (82.44%), Space: 117.5 MB (88.15%) - LeetHub

--- 2640-maximum-number-of-integers-to-choose-from-a-range-i/2640-maximum-number-of-integers-to-choose-from-a-range-i.cpp
+++ 2640-maximum-number-of-integers-to-choose-from-a-range-i/2640-maximum-number-of-integers-to-choose-from-a-range-i.cpp
@@ -1,10 +1,13 @@
 class Solution {
 public:
     int maxCount(vector<int>& banned, int n, int maxSum) {
-        int ans = 0;
-    unordered_set<int> s(begin(banned), end(banned));
-    for(int i = 1; i <= n; ++i){
-        if(s.find(i) == s.end() && maxSum - i >= 0) { ans++; maxSum -= i; }
+        sort(banned.begin(), banned.end());
+    int ans = 0;
+    for(int i = 1, j = 0; i <= n; ++i){
+        if((j == banned.size() || i < banned[j] ) && maxSum- i >= 0) { 
+            ans++; maxSum -= i; 
+        }
+        while(j < banned.size() && i >= banned[j]) j++;
     }
     return ans;
     }
