0042d3585b1d13a0c8eb17432a7c72ac1ec8d76e
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-12-09 21:55:26 +0530

    Time: 11 ms (96.98%), Space: 61.2 MB (6.04%) - LeetHub

--- 2356-largest-combination-with-bitwise-and-greater-than-zero/2356-largest-combination-with-bitwise-and-greater-than-zero.cpp
+++ 2356-largest-combination-with-bitwise-and-greater-than-zero/2356-largest-combination-with-bitwise-and-greater-than-zero.cpp
@@ -1,32 +1,13 @@
 class Solution {
 public:
-    int largestCombination(vector<int>& candidates) {
-        vector<int>bits(32,0);
-        int j = 0;
-        for(auto &i:candidates){
-            j = 0;
-            bits[0] += (i&1);
-            i = i>>1;
-            j++;
-            while(i){
-                
-                
-                bits[j] += i&1;
-                j++;
-                i = i>>1;
-
-            }
-            
-
-            
+    int largestCombination(vector<int>& c) {
+        int ret = 0;
+        vector<int> bs(24);
+        for (auto& n : c) {
+            int nn = n;
+            for (int i = 0; i < bs.size(); i++, nn >>= 1) bs[i] += nn & 1;
         }
-
-        int ans = bits[0];
-        for( int i = 1; i<32; i++){
-            if(bits[i]>ans){
-                ans = bits[i];
-            }
-        }
-        return ans;
+        for(auto& b:bs) if(ret < b) ret = b;
+        return ret;
     }
 };
