1cc893cf964b12019923ca26475b312d94254353
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-12-09 21:49:02 +0530

    Time: 45 ms (22.66%), Space: 137.8 MB (7.72%) - LeetHub

--- 3427-special-array-ii/3427-special-array-ii.cpp
+++ 3427-special-array-ii/3427-special-array-ii.cpp
@@ -3,10 +3,7 @@ public:
     vector<bool> isArraySpecial(vector<int>& nums, vector<vector<int>>& queries) {
         vector<bool> ans;
     vector<int> converted(1, 0);
-    for(int i = 1, j = 0; i < nums.size(); ++i){
-        if( (nums[i]%2) == (nums[i-1]%2) ) j++;
-        converted.push_back(j);
-    }
+    for(int i = 1; i < nums.size(); ++i) converted.push_back(converted.back() + (nums[i]%2 == nums[i-1]%2));
     for(auto q: queries) ans.push_back( converted[q[0]] == converted[q[1]] );
     return ans;
     }
