bf23d578a08c739966e9eb35a19f8fc1d9edb0fe
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-12-11 22:27:57 +0530

    Time: 8 ms (96.68%), Space: 115.4 MB (39.28%) - LeetHub

--- 2891-maximum-beauty-of-an-array-after-applying-operation/2891-maximum-beauty-of-an-array-after-applying-operation.cpp
+++ 2891-maximum-beauty-of-an-array-after-applying-operation/2891-maximum-beauty-of-an-array-after-applying-operation.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     int maximumBeauty(vector<int>& nums, int k) {
-        sort(nums.begin(),nums.end());
-        int start = 0, ans = 0;
-        for(int i=0; i<nums.size(); i++){
-            // here nums[i] = maxi and nums[start] = mini
-            // while window is not vaild increase the current minimum 
-            while(nums[i]-nums[start] > 2*k)
-                start++;
-            // the current window i.e. [start ... i] is valid so consider it
-            ans = max(ans,i-start+1);
+        int m = *max_element(nums.begin(), nums.end()) + k * 2 + 2;
+        vector<int> d(m);
+        for (int x : nums) {
+            d[x]++;
+            d[x + k * 2 + 1]--;
+        }
+        int ans = 0, s = 0;
+        for (int x : d) {
+            s += x;
+            ans = max(ans, s);
         }
         return ans;
     }
