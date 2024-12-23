0296659348ba4c0ab4eba685f402a3ae87bb1e1b
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-12-23 22:20:46 +0530

    Time: 24 ms (99.74%), Space: 147.7 MB (99.63%) - LeetHub

--- 3058-maximum-number-of-k-divisible-components/3058-maximum-number-of-k-divisible-components.cpp
+++ 3058-maximum-number-of-k-divisible-components/3058-maximum-number-of-k-divisible-components.cpp
@@ -1,34 +1,38 @@
-const int N = 3e4 + 10;
-int e[N<<1],ne[N<<1],h[N];
-int idx =0 ;
-void add(int u,int v){
-    e[idx] = v;
-    ne[idx] = h[u];
-    h[u] = idx++;
-}
-typedef long long ll;
+const int MAX_N = 3e4+5;
+vector<int> adj[MAX_N];
+bool visited[MAX_N];
+
 class Solution {
+    int ans = 0;
+
+    void prune(int n) {
+        for (int i = 0; i < n; ++i) adj[i].clear();
+    }
+
+    void buildGraph(vector<vector<int>>& edges) {
+        for (const auto &edge: edges) 
+            adj[edge[0]].emplace_back(edge[1]),
+            adj[edge[1]].emplace_back(edge[0]);
+    }
+
+    long long dfs(int node, int parent, vector<int>& values, int k) {
+        long long sum = values[node];
+        for (const int &neighbor: adj[node]) {
+            if (neighbor == parent) continue;
+            sum += dfs(neighbor, node, values, k);
+            sum %= k;
+        }
+        const bool sum_divisible_by_k = sum % k == 0;
+        ans += sum_divisible_by_k;
+        return sum_divisible_by_k ? 0 : sum;
+    }
+    
 public:
     int maxKDivisibleComponents(int n, vector<vector<int>>& edges, vector<int>& values, int k) {
-        memset(h,-1,sizeof h);
-        idx = 0;
-        for(auto& e : edges){
-            add(e[0], e[1]);
-            add(e[1], e[0]);
-        }
-        int ans= 0;
-        auto dfs = [&](auto && dfs,int u,int fa) -> ll{
-            ll res = values[u];
-            for(int i = h[u]; i != -1; i = ne[i]){
-                int v = e[i];
-                if(v != fa){
-                    res += dfs(dfs,v,u);
-                }
-            }
-            ans += res%k == 0;
-            return res;
-        };
-        dfs(dfs,0,-1);
+        prune(n);
+        buildGraph(edges);
+        
+        dfs(0, -1, values, k);
         return ans;
     }
 };
