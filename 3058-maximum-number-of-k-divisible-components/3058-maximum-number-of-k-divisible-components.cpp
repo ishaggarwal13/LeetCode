ad888989e0b36217e1a095e0cd3a4a636aa9a894
Isha Aggarwal <aggarwalisha1303@gmail.com>
2024-12-23 22:22:08 +0530

    Time: 68 ms (99.09%), Space: 168.3 MB (95.6%) - LeetHub

--- 3058-maximum-number-of-k-divisible-components/3058-maximum-number-of-k-divisible-components.cpp
+++ 3058-maximum-number-of-k-divisible-components/3058-maximum-number-of-k-divisible-components.cpp
@@ -1,38 +1,29 @@
-const int MAX_N = 3e4+5;
-vector<int> adj[MAX_N];
-bool visited[MAX_N];
-
 class Solution {
-    int ans = 0;
-
-    void prune(int n) {
-        for (int i = 0; i < n; ++i) adj[i].clear();
-    }
-
-    void buildGraph(vector<vector<int>>& edges) {
-        for (const auto &edge: edges) 
-            adj[edge[0]].emplace_back(edge[1]),
-            adj[edge[1]].emplace_back(edge[0]);
-    }
-
-    long long dfs(int node, int parent, vector<int>& values, int k) {
-        long long sum = values[node];
-        for (const int &neighbor: adj[node]) {
-            if (neighbor == parent) continue;
-            sum += dfs(neighbor, node, values, k);
-            sum %= k;
-        }
-        const bool sum_divisible_by_k = sum % k == 0;
-        ans += sum_divisible_by_k;
-        return sum_divisible_by_k ? 0 : sum;
-    }
-    
 public:
     int maxKDivisibleComponents(int n, vector<vector<int>>& edges, vector<int>& values, int k) {
-        prune(n);
-        buildGraph(edges);
-        
-        dfs(0, -1, values, k);
+        std::vector<std::vector<int>> adj(n);
+        for (int i = 0; i < n; ++i) {
+            adj[i].reserve(7);
+        }
+        for (const auto& edge : edges) {
+            adj[edge[0]].push_back(edge[1]);
+            adj[edge[1]].push_back(edge[0]);
+        }
+        int ans = 0;
+        auto dfs = [&](auto&& self, int v, int p = -1) -> int {
+            int sm = 0;
+            for (int u : adj[v]) {
+                if (u != p) {
+                    sm += self(self, u, v);
+                    sm %= k;
+                }
+            }
+            if ((sm + values[v]) % k == 0) {
+                ++ans;
+            }
+            return sm + values[v];
+        };
+        dfs(dfs, 0);
         return ans;
     }
 };
