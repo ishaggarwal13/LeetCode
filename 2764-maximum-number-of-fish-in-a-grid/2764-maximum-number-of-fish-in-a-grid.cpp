92316020234a89b769d324555f9abd3ce906c68c
Isha Aggarwal <aggarwalisha1303@gmail.com>
2025-01-28 21:35:01 +0530

    Time: 86 ms (6.13%), Space: 115.5 MB (6.66%) - LeetHub

--- 2764-maximum-number-of-fish-in-a-grid/2764-maximum-number-of-fish-in-a-grid.cpp
+++ 2764-maximum-number-of-fish-in-a-grid/2764-maximum-number-of-fish-in-a-grid.cpp
@@ -5,33 +5,31 @@ public:
         int row = grid.size();
         int col = grid[0].size();
 
-        vector<vector<bool>> visited(row, vector<bool>(col, false));
-        vector<vector<int>> directions = {{0,1}, {0,-1}, {-1,0}, {1,0}};
-
         for(int i=0; i<row; i++){
             for(int j=0; j<col; j++){
                 //check if cell is not 0 and cell is not visisted
-                if(grid[i][j] > 0 && !visited[i][j]){
-                    int catchFish = dfs(grid, visited, directions, i, j);
-                    maxFish = max(maxFish, catchFish);
+                if(grid[i][j] > 0){
+                    maxFish = max(maxFish, dfs(grid, i, j));
                 }
             }
         }
         return maxFish;
     }
 
-    int dfs(vector<vector<int>>& grid, vector<vector<bool>>& visited, vector<vector<int>>& directions, int row, int col){
-        if(row<0 || row>=grid.size() || col<0 || col>=grid[0].size() || visited[row][col] || grid[row][col] == 0){
+    int dfs(vector<vector<int>>& grid, int row, int col){
+        if(row<0 || row>=grid.size() || col<0 || col>=grid[0].size() || grid[row][col] == 0){
             return 0;
         }
 
-        visited[row][col] = true;
         int total = grid[row][col];
+        grid[row][col] = 0;
+
+        vector<vector<int>> directions = {{0,1}, {0,-1}, {-1,0}, {1,0}};
 
         for(const auto& it : directions){
             int newRow = row + it[0];
             int newCol = col + it[1];
-            total += dfs(grid, visited, directions, newRow, newCol);
+            total += dfs(grid, newRow, newCol);
         }
         return total;
     }
