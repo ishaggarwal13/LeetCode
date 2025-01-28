67b14a9deb93042f8a4677adb4b14084dd6f2d77
Isha Aggarwal <aggarwalisha1303@gmail.com>
2025-01-28 22:08:12 +0530

    Time: 12 ms (47.81%), Space: 108 MB (24.52%) - LeetHub

--- 2764-maximum-number-of-fish-in-a-grid/2764-maximum-number-of-fish-in-a-grid.cpp
+++ 2764-maximum-number-of-fish-in-a-grid/2764-maximum-number-of-fish-in-a-grid.cpp
@@ -1,21 +1,5 @@
 class Solution {
 public:
-    int dfs(vector<vector<int>>& grid, int r, int c){
-        if(r<0 || r>=grid.size() || c<0 || c>=grid[0].size() || grid[r][c] == 0){
-            return 0;
-        }
-
-        int total = grid[r][c];
-        grid[r][c] = 0;
-
-        total += dfs(grid, r+1, c);
-        total += dfs(grid, r, c+1);
-        total += dfs(grid, r-1, c);
-        total += dfs(grid, r, c-1);
-
-        return total;
-    }
-
     int findMaxFish(vector<vector<int>>& grid) {
         int maxFish=0;
         int row = grid.size();
@@ -23,9 +7,29 @@ public:
 
         for(int i=0; i<row; i++){
             for(int j=0; j<col; j++){
-                //check if cell is not 0 and cell is not visisted
                 if(grid[i][j] > 0){
-                    maxFish = max(maxFish, dfs(grid, i, j));
+                    int total = 0;
+                    queue<pair<int, int>> q;
+                    q.push({i, j});
+
+                    while(!q.empty()){
+                        auto[r,c] = q.front();
+                        q.pop();
+                        if(grid[r][c] == 0) continue;
+                        total += grid[r][c];
+                        grid[r][c] = 0;
+
+                        int dir[] = {1, 0, -1, 0, 1};
+                        for(int k=0; k<4; k++){
+                            int nr = r + dir[k];
+                            int nc = c + dir[k+1];
+
+                            if(nr>=0 && nr<row && nc>=0 && nc<col && grid[nr][nc]>0){
+                                q.push({nr, nc});
+                            }
+                        }
+                    }
+                    maxFish = max(maxFish, total);
                 }
             }
         }
