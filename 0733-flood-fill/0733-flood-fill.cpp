8618eb2d99ae3870f1d13236eb8b04f5fd6d6ed6
Isha Aggarwal <aggarwalisha1303@gmail.com>
2025-01-29 23:38:41 +0530

    Time: 0 ms (100%), Space: 18.4 MB (34.86%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,18 +1,33 @@
 class Solution {
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int n = image.size(), m = image[0].size();
-        dfs(image, sr, sc, n, m, image[sr][sc], color);
+        dfs(image, sr, sc, color);
         return image;
     }
 
-    void dfs(vector<vector<int>>& image, int sr, int sc, int n, int m, int target, int color){
-        if(sr<0 || sc<0 || sr==n || sc==m || image[sr][sc] == color || image[sr][sc] != target) return;
-        image[sr][sc] = color;
-        dfs(image, sr+1, sc, n, m, target, color);
-        dfs(image, sr-1, sc, n, m, target, color);
-        dfs(image, sr, sc+1, n, m, target, color);
-        dfs(image, sr, sc-1, n, m, target, color);
+    void dfs(vector<vector<int>>& image, int sr, int sc, int color){
+        if(image[sr][sc] == color) return; 
+        int main = image[sr][sc];
+        queue<pair<int,int>> q;
+        q.push({sr, sc});
 
+        while(!q.empty()){
+            pair<int,int>p = q.front();
+            q.pop();
+
+            int r = p.first;
+            int c = p.second;
+
+            if(r < 0 || r >= image.size() || c < 0 || c >= image[0].size() 
+            || image[r][c] != main) {
+                continue;
+            }
+
+            image[r][c] = color;
+            q.push({r-1,c});
+            q.push({r+1,c});
+            q.push({r,c-1});
+            q.push({r,c+1});
+        }
     }
 };
