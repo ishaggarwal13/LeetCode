463980aa8da480e5e97114afa757ca5a7a669f36
Isha Aggarwal <aggarwalisha1303@gmail.com>
2025-01-31 01:43:38 +0530

    Time: 117 ms (91.63%), Space: 41.4 MB (92.47%) - LeetHub

--- 2583-divide-nodes-into-the-maximum-number-of-groups/2583-divide-nodes-into-the-maximum-number-of-groups.cpp
+++ 2583-divide-nodes-into-the-maximum-number-of-groups/2583-divide-nodes-into-the-maximum-number-of-groups.cpp
@@ -1,69 +1,65 @@
-#include <ranges>
-
 class Solution {
 public:
-    int magnificentSets(int n, vector<vector<int>>& edges) {
-    auto adj = vector<vector<int>>(n);
-    for (const auto& edge : edges) {
-      auto [u, v] = make_pair(edge.front(), edge.back());
-      adj[u - 1].push_back(v - 1);
-      adj[v - 1].push_back(u - 1);
-    }
-
-    auto node_color = vector<int>(n, uncolored);
-    auto ccs = vector<vector<int>>{};
-    for (auto u : views::iota(0, n)) {
-      if (node_color[u] != uncolored) continue;
-      if (!dfs(u, adj, black, node_color, ccs.emplace_back())) return -1;
-    }
-
-    return transform_reduce(begin(ccs), end(ccs), 0, plus{},
-        [&adj](const auto& cc) { return max_groups_for_cc(cc, adj); } );
-  }
-private:
-  static constexpr auto uncolored = -1;
-  static constexpr auto black = 0;
-  static constexpr auto white = 1;
-
-  static bool dfs(int u, const vector<vector<int>>& adj, int color,
-            vector<int>&node_color, vector<int>& cc) {
-    auto complement = !color;
-    node_color[u] = color;
-    cc.push_back(u);
-    for (auto v : adj[u]) {
-      if (node_color[v] == color ||
-          (node_color[v] == uncolored &&
-           !dfs(v, adj, complement, node_color, cc)))
-        return false;
-    }
-    return true;
-  }
-
-  static int max_groups_for_cc(const vector<int>& cc,
-      const vector<vector<int>>& adj) {
-    if (size(cc) == 1) return 1;
-
-    auto q = vector<int>{};
-    auto next_q = vector<int>{};
-    auto max_groups = 0;
-    for (auto start : cc) {
-      q.push_back(start);
-      auto seen = bitset<500>{1} << start;
-      auto groups = 0;
-      while (!empty(q)) {
-        ++groups;
-        for (auto u : q) {
-          for (auto v : adj[u]) {
-            if (seen[v]) continue;
-            seen.set(v);
-            next_q.push_back(v);
-          }
+    vector<int>adj[505];
+    int col[505];
+    int dis[505][505];
+    bool flag;
+	//nodes in a connected graph.
+    vector<int>kara;
+	
+	//check if graph is bipartite
+    void dfs(int u,int c=1){
+        col[u] = c;
+        kara.push_back(u);
+        for(int&v:adj[u]){
+            if(!col[v]) dfs(v,3-c);
+            else if(col[v]==c)flag = 0;// graph is not bipartite
         }
-        swap(q, next_q);
-        next_q.clear();
-      }
-      if (groups > max_groups) max_groups = groups;
     }
-    return max_groups;
-  }
+	//calculate minimum distance between all nodes.
+    void bfs(int src){
+        for(int i=1;i<=500;i++)dis[src][i] = 100001;
+        dis[src][src] = 1;
+        queue<int>q;
+        q.push(src);
+        while(q.size()){
+            auto u = q.front();
+            q.pop();
+            for(int&v:adj[u]){
+                if(dis[src][v]>dis[src][u]+1){
+                    dis[src][v] = dis[src][u]+1;
+                    q.push(v);
+                }
+            }
+        }
+    }
+    int magnificentSets(int n, vector<vector<int>>& edges) {
+        for(int i=1;i<=n;i++)adj[i].clear();
+        memset(col,0,sizeof col);
+        for(auto&e:edges){
+            adj[e[0]].push_back(e[1]);
+            adj[e[1]].push_back(e[0]);
+        }
+        
+        for(int i=1;i<=n;i++)bfs(i);
+        int ans = 0;
+        int in = 0;
+        for(int i=1;i<=n;i++){
+            if(col[i]==0){
+                kara.clear();
+                flag = 1;
+                in++;
+                dfs(i);
+                if(!flag) return -1;
+                int mxdis = 0;
+                for(int&i:kara){
+                        for(int&j:kara){
+                            mxdis=max(mxdis,dis[i][j]);
+                        }
+                }
+                ans+=mxdis;
+            }
+        }
+        return ans;
+    }
 };
