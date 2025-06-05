c4ccda9f65d507b36db9050989117e51ad483684
Isha Aggarwal <aggarwalisha1303@gmail.com>
2025-06-05 23:42:05 +0530

    Time: 15 ms (18.55%), Space: 11.5 MB (15.96%) - LeetHub

--- 1058-lexicographically-smallest-equivalent-string/1058-lexicographically-smallest-equivalent-string.cpp
+++ 1058-lexicographically-smallest-equivalent-string/1058-lexicographically-smallest-equivalent-string.cpp
@@ -1,26 +1,36 @@
 class Solution {
 public:
     string smallestEquivalentString(string s1, string s2, string baseStr) {
-        // using array with replacing the largest of two char to teh lowest one
-        char ch[26];
-        for(int i=0; i<26; i++){
-            ch[i] = 'a' + i;
+        //building teh graph and then use DFS to find smalledt lexi char
+        int n = s1.size();
+        unordered_map<char, vector<char>> adj;
+
+        //building graph
+        for(int i=0; i<n; i++){
+            char u = s1[i];
+            char v = s2[i];
+            adj[u].push_back(v);
+            adj[v].push_back(u);
         }
 
-        for(int i=0; i<s1.size(); i++){
-            char toreplace = max(ch[s1[i]-'a'], ch[s2[i]-'a']);
-            char replacewith = min(ch[s1[i]-'a'], ch[s2[i]-'a']);
-            for(int i=0; i<26; i++){
-                if(ch[i] == toreplace){
-                    ch[i] = replacewith;
-                }
+        // replace char with smallest
+        string result;
+        for(auto base : baseStr){
+            vector<int> vis(26, 0);
+            char miniChar = dfs(adj, base, vis);
+            result.push_back(miniChar);
+        }
+        return result;
+    }
+
+    char dfs(unordered_map<char, vector<char>>& adj, char base, vector<int>& vis){
+        vis[base - 'a'] = 1;
+        char miniChar = base;
+        for(char u : adj[base]){
+            if(vis[u-'a'] == 0){
+                miniChar = min(miniChar, dfs(adj, u, vis));
             }
         }
-
-        for(int i=0; i<baseStr.size(); i++){
-            baseStr[i] = ch[baseStr[i] - 'a'];
-        }
-
-        return baseStr;
+        return miniChar;
     }
 };
