d4a5ebd517299e0cc57eacf719310a77a215faad
Isha Aggarwal <aggarwalisha1303@gmail.com>
2025-06-15 23:50:46 +0530

    Time: 0 ms (100%), Space: 8.4 MB (20.85%) - LeetHub

--- 1529-max-difference-you-can-get-from-changing-an-integer/1529-max-difference-you-can-get-from-changing-an-integer.cpp
+++ 1529-max-difference-you-can-get-from-changing-an-integer/1529-max-difference-you-can-get-from-changing-an-integer.cpp
@@ -1,28 +1,65 @@
-#include <iostream>
-#include <set>
-#include <string>
-#include <algorithm>
-
-using namespace std;
-
 class Solution {
 public:
     int maxDiff(int num) {
-        string numStr = to_string(num);
-        set<char> uniqueDigits(numStr.begin(), numStr.end());
-        int maxVal = num, minVal = num;
 
-        for (char digit : uniqueDigits) {
-            for (char newDigit = '0'; newDigit <= '9'; ++newDigit) {
-                if (numStr[0] == digit && newDigit == '0') continue;
-                string newNumStr = numStr;
-                replace(newNumStr.begin(), newNumStr.end(), digit, newDigit);
-                int newNum = stoi(newNumStr);
-                maxVal = max(maxVal, newNum);
-                minVal = min(minVal, newNum);
-            }
+        string t = to_string(num);
+        // cout<<t;
+        char c;
+
+        bool f = false;
+
+        for (int i = 0; i < t.size(); i++) {
+
+            if (t[i] == '9')
+                continue;
+
+            c = t[i];
+            break;
+        }
+        // cout<<t;
+        string p = to_string(num);
+
+        for (int i = 0; i < t.size(); i++) {
+            if (t[i] == c)
+                t[i] = '9';
         }
 
-        return maxVal - minVal;
+        // cout<<t<<" ";
+
+        char c1 = p[0];
+        for (int i = 0; i < t.size(); i++) {
+            if (c1 == p[i]) {
+                if (p[i] == '1')
+                    continue;
+                p[i] = '1';
+                f = true;
+            }
+        }
+        if (f) {
+            int p1 = stoi(p);
+            int p2 = stoi(t);
+
+            cout << p1;
+            return p2 - p1;
+        }
+        // char c=
+
+        char c3;
+        // char c
+
+        for (int i = 0; i < p.size(); i++) {
+            if (p[i] != '0' && i != 0 && p[i]!=p[0]) {
+                c3 = p[i];
+                break;
+            }
+        }
+        for (int i = 0; i < p.size(); i++) {
+            if (c3 == p[i] && i!=0)
+                p[i] = '0';
+        }
+
+        cout << p;
+
+        return stoi(t) - stoi(p);
     }
 };
