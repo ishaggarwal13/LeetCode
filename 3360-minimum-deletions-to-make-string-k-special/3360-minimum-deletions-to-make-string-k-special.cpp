243d0e83a2668f6f4e05784a57c251ab62b39bf4
Isha Aggarwal <aggarwalisha1303@gmail.com>
2025-06-22 00:00:32 +0530

    Time: 20 ms (28.99%), Space: 19.8 MB (31.16%) - LeetHub

--- 3360-minimum-deletions-to-make-string-k-special/3360-minimum-deletions-to-make-string-k-special.cpp
+++ 3360-minimum-deletions-to-make-string-k-special/3360-minimum-deletions-to-make-string-k-special.cpp
@@ -1,39 +1,18 @@
 class Solution {
 public:
     int minimumDeletions(string word, int k) {
-        unordered_map<char, int> freq;
-        for(char w : word){
-            freq[w]++;
+        unordered_map<char,int>mp;
+        int n = word.size(), count = INT_MAX , mini = INT_MAX;
+        for (char c:word){
+            mp[c]++;
         }
-
-        vector<int> frequencies;
-        for(auto& pair: freq){
-            frequencies.push_back(pair.second);
+        for (auto pair: mp){
+            int curr = 0; 
+            for (auto m : mp){
+                curr += (m.second<pair.second) ? m.second : max(0, m.second-(pair.second+k));
+            }
+            count=min(count,curr);
         }
-
-        sort(frequencies.begin(), frequencies.end());
-
-        int minDeletions = INT_MAX;
-        int n = frequencies.size();
-        
-        for (int i = 0; i < n; ++i) {
-            int base = frequencies[i];
-            int totalDeletions = 0;
-
-            for (int j = 0; j < i; ++j) {
-                totalDeletions += frequencies[j];
-            }
-            
-            for (int j = i; j < n; ++j) {
-                if (frequencies[j] > base + k) {
-                    totalDeletions += frequencies[j] - (base + k);
-                }
-            }
-            
-            if (totalDeletions < minDeletions) {
-                minDeletions = totalDeletions;
-            }
-        }
-        return minDeletions;
+        return count;
     }
 };
