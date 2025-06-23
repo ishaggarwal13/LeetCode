e2b67ddfd35d8cf5345955a98ef4542c7b8b3de0
Isha Aggarwal <aggarwalisha1303@gmail.com>
2025-06-23 19:45:43 +0530

    Time: 43 ms (28.93%), Space: 13.9 MB (93.09%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        //brute force O(N^2) NEED TO OPTIMIZE IN ONE PASS
         for(int i=0; i<nums.size()-1; i++){
             for(int j =i+1; j<nums.size(); j++){
                 if(nums[i] + nums[j] == target){
