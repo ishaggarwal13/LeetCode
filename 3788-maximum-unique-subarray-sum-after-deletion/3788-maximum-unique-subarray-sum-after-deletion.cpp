e8d7c583665402e84aad29de0f16be0f1d9a37b7
Isha Aggarwal <aggarwalisha1303@gmail.com>
2025-07-25 19:30:33 +0530

    Time: 1 ms (37.33%), Space: 29.4 MB (75.17%) - LeetHub

--- 3788-maximum-unique-subarray-sum-after-deletion/3788-maximum-unique-subarray-sum-after-deletion.cpp
+++ 3788-maximum-unique-subarray-sum-after-deletion/3788-maximum-unique-subarray-sum-after-deletion.cpp
@@ -2,19 +2,30 @@ class Solution {
 public:
     int maxSum(vector<int>& nums) {
         bool allNegative = true;
-        int maxVal = INT_MIN;
-
+        int maxValue = INT_MIN;
         for (int n : nums) {
-            if (n >= 0) allNegative = false;
-            if (n > maxVal) maxVal = n;
+            if (n >= 0) {
+                allNegative = false;
+            }
+            if (n > maxValue) {
+                maxValue = n;
+            }
         }
-        if (allNegative) return maxVal;
+        if (allNegative)
+            return maxValue;
 
-        unordered_set<int> unique(nums.begin(), nums.end());
+        bool seen[101] = {false};
+        for (int n : nums) {
+            if (n >= 0 && n < 101) {
+                seen[n] = true;
+            }
+        }
 
         int sum = 0;
-        for (int val : unique) {
-            if (val > 0) sum += val;
+        for (int i = 1; i < 101; i++) {
+            if (seen[i]) {
+                sum += i;
+            }
         }
 
         return sum;
