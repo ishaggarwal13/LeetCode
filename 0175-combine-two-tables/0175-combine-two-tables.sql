93fdb325596350690f7ec5c92c50332e8ec0ab28
Isha Aggarwal <aggarwalisha1303@gmail.com>
2026-05-04 15:04:58 +0530

    Time: 1836 ms (5%), Space: 0B (100%) - LeetHub

--- 0175-combine-two-tables/0175-combine-two-tables.sql
+++ 0175-combine-two-tables/0175-combine-two-tables.sql
@@ -1,3 +1,5 @@
-/* Write your T-SQL query statement below */
-select p.firstName , p.lastName, a.city , a.state 
-    from Person p LEFT JOIN Address a on p.personId  = a.personId  
+# Write your MySQL query statement below
+select p.firstName, p.lastName, a.city, a.state
+from Person p
+left join Address a
+on p.personId = a.personId
